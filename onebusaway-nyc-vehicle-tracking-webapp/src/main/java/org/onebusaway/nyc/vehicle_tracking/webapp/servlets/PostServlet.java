dc29c8f69bd4de09cfa83059ca6645d6a794f0ab
novalis@novalis.org <novalis@novalis.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-03 20:56:51 +0000

    some basic work on reading siri (wrong, but interesting)
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2455 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-vehicle-tracking-webapp/src/main/java/org/onebusaway/nyc/vehicle_tracking/webapp/servlets/PostServlet.java
+++ onebusaway-nyc-vehicle-tracking-webapp/src/main/java/org/onebusaway/nyc/vehicle_tracking/webapp/servlets/PostServlet.java
@@ -1,5 +1,15 @@
 package org.onebusaway.nyc.vehicle_tracking.webapp.servlets;
 
+import org.onebusaway.siri.model.ServiceDelivery;
+import org.onebusaway.siri.model.Siri;
+import org.onebusaway.siri.model.VehicleActivity;
+import org.onebusaway.siri.model.VehicleLocation;
+import org.onebusaway.transit_data_federation.services.realtime.TripPositionService;
+
+import com.thoughtworks.xstream.XStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -11,16 +21,30 @@ import javax.servlet.http.HttpServletResponse;
 public class PostServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
-
+  private XStream xstream;
+  @Autowired
+  TripPositionService positionService;
+  
+  @Override
+  public void init() {
+    xstream = new XStream();
+    xstream.processAnnotations(Siri.class);
+    xstream.processAnnotations(VehicleActivity.class);
+  }
+  
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
     BufferedReader reader = req.getReader();
-    String line = null;
-
-    while ((line = reader.readLine()) != null) {
-      System.err.println(line);
+    Siri siri = (Siri) xstream.fromXML(reader);
+    ServiceDelivery delivery = siri.ServiceDelivery;
+    VehicleActivity vehicleActivity = delivery.VehicleMonitoringDelivery.deliveries.get(0);
+    VehicleLocation location = vehicleActivity.MonitoredVehicleJourney.VehicleLocation;
+    /* insert into */
+    if (positionService == null) {
+      System.out.println("wrong!");
     }
+    
   }
 }
