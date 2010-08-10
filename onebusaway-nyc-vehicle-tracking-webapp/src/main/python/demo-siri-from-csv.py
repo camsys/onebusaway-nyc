3850dd68cdf9246c8103b1ef4ad560597d1fc239
novalis@novalis.org <novalis@novalis.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-10 18:14:08 +0000

    quick demo of siri reading code
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2508 638cd66d-8850-0410-a654-37fb4b726c3a

---
+++ onebusaway-nyc-vehicle-tracking-webapp/src/main/python/demo-siri-from-csv.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+#demonstrate that onebusaway can read data delivered in SIRI format.
+
+import csv
+import httplib
+from datetime import datetime, timedelta
+from tz import Eastern
+
+#the siri template
+f = open("siri.xml")
+siri_template = f.read()
+f.close()
+
+#the test data (for the b62)
+f = open("../../../../onebusaway-nyc-vehicle-tracking/src/test/resources/org/onebusaway/nyc/vehicle_tracking/impl/inference/ivn-dsc.csv")
+reader = csv.reader(f)
+
+#how Siri wants dates formatte
+siriDateFormat = "%Y-%m-%dT%H:%M:%S%Z"
+
+firstRead = False
+fields = ["vid","date","time","lat","lon","dt","dsc","new.dsc"]
+for line in reader:
+    if not firstRead:
+        #the first row is a header
+        firstRead = True
+        continue
+    row = dict((fields[i], line[i]) for i in range(len(line)))
+    #munge date formats
+    now = datetime.strptime(row['dt'], "%Y-%m-%d %H:%M:%S")
+    now = now.replace(tzinfo = Eastern)
+    row['dt'] = now.strftime(siriDateFormat)
+    validUntil = now + timedelta(0,60,0) #one minute later
+    row['validUntil'] = validUntil.strftime(siriDateFormat)
+    row['date'] = now.strftime("%Y-%m-%d")
+    
+    request = siri_template % row
+
+    #generate request
+    conn = httplib.HTTPConnection('localhost:9180')
+
+    conn.request("POST", "/.../update_location", request, {})
+
+    response = conn.getresponse()
+    data = response.read()
+    if data != "ok":
+        print "error sending data %s" % row
+
---
+++ onebusaway-nyc-vehicle-tracking-webapp/src/main/python/tz.py
@@ -0,0 +1,64 @@
+# For some idiotic reason, Python doesn't have timezones built in.
+# This implementation of the basic US timezone system is copied from
+# the Python docs.
+
+from datetime import tzinfo, timedelta, datetime
+
+ZERO = timedelta(0)
+HOUR = timedelta(hours=1)
+
+# A complete implementation of current DST rules for major US time zones.
+
+def first_sunday_on_or_after(dt):
+    days_to_go = 6 - dt.weekday()
+    if days_to_go:
+        dt += timedelta(days_to_go)
+    return dt
+
+# In the US, DST starts at 2am (standard time) on the first Sunday in April.
+DSTSTART = datetime(1, 4, 1, 2)
+# and ends at 2am (DST time; 1am standard time) on the last Sunday of Oct.
+# which is the first Sunday on or after Oct 25.
+DSTEND = datetime(1, 10, 25, 1)
+
+class USTimeZone(tzinfo):
+
+    def __init__(self, hours, reprname, stdname, dstname):
+        self.stdoffset = timedelta(hours=hours)
+        self.reprname = reprname
+        self.stdname = stdname
+        self.dstname = dstname
+
+    def __repr__(self):
+        return self.reprname
+
+    def tzname(self, dt):
+        if self.dst(dt):
+            return self.dstname
+        else:
+            return self.stdname
+
+    def utcoffset(self, dt):
+        return self.stdoffset + self.dst(dt)
+
+    def dst(self, dt):
+        if dt is None or dt.tzinfo is None:
+            # An exception may be sensible here, in one or both cases.
+            # It depends on how you want to treat them.  The default
+            # fromutc() implementation (called by the default astimezone()
+            # implementation) passes a datetime with dt.tzinfo is self.
+            return ZERO
+        assert dt.tzinfo is self
+
+        # Find first Sunday in April & the last in October.
+        start = first_sunday_on_or_after(DSTSTART.replace(year=dt.year))
+        end = first_sunday_on_or_after(DSTEND.replace(year=dt.year))
+
+        # Can't compare naive to aware objects, so strip the timezone from
+        # dt first.
+        if start <= dt.replace(tzinfo=None) < end:
+            return HOUR
+        else:
+            return ZERO
+
+Eastern  = USTimeZone(-5, "Eastern",  "-5:00", "-4:00")
