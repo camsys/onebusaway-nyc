ae5f7bab7bb0f3777c29398f84866b7437a5516e
bdferris@onebusaway.org <bdferris@onebusaway.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-10 18:38:26 +0000

    Fix unit test
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2509 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/TestVehicleLocationInferenceServiceImpl.java
+++ onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/TestVehicleLocationInferenceServiceImpl.java
@@ -1,7 +1,14 @@
 package org.onebusaway.nyc.vehicle_tracking.impl.inference;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
 import org.onebusaway.gtfs.csv.CsvEntityContext;
 import org.onebusaway.gtfs.csv.CsvEntityReader;
 import org.onebusaway.gtfs.csv.EntityHandler;
@@ -11,73 +18,16 @@ import org.onebusaway.gtfs.csv.schema.AbstractFieldMapping;
 import org.onebusaway.gtfs.csv.schema.BeanWrapper;
 import org.onebusaway.gtfs.csv.schema.DefaultEntitySchemaFactory;
 import org.onebusaway.gtfs.csv.schema.EntitySchemaFactoryHelper;
-import org.onebusaway.gtfs.csv.schema.annotations.CsvFields;
 import org.onebusaway.gtfs.csv.schema.beans.CsvEntityMappingBean;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.nyc.vehicle_tracking.services.VehicleLocationInferenceRecord;
 import org.onebusaway.realtime.api.VehicleLocationRecord;
 
-import org.junit.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Map;
-
-@CsvFields(filename = "ivn-dsc.csv", fieldOrder = {
-    "vehicleId", "date", "time", "lat", "lon",
-    "timestamp", "dsc", "new_dsc"})
-class NycTestLocationRecord {
-  private String vehicleId;
-  
-  private double lat;
-  private double lon;
-  private long timestamp;
-  private String dsc;
-  
-  public void setVehicleId(String vehicleId) {
-    this.vehicleId = vehicleId;
-  }
-  public String getVehicleId() {
-    return vehicleId;
-  }
-  public void setLat(double lat) {
-    this.lat = lat;
-  }
-  public double getLat() {
-    return lat;
-  }
-  public void setLon(double lon) {
-    this.lon = lon;
-  }
-  public double getLon() {
-    return lon;
-  }
-  public void setTimestamp(String timestamp) {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
-    try {
-      this.timestamp = sdf.parse(timestamp).getTime();
-    } catch (ParseException e) {
-      throw new RuntimeException("error parsing datetime " + timestamp, e);
-    }
-  }
-  public long getTimestamp() {
-    return timestamp;
-  }
-  public void setDsc(String dsc) {
-    this.dsc = dsc;
-  }
-  public String getDsc() {
-    return dsc;
-  } 
-}
-
 public class TestVehicleLocationInferenceServiceImpl {
 
   @Test
-  public void testInference() throws CsvEntityIOException, IOException, InterruptedException {
+  public void testInference() throws CsvEntityIOException, IOException,
+      InterruptedException {
     final VehicleLocationInferenceServiceImpl service;
     service = new VehicleLocationInferenceServiceImpl();
     service.start();
@@ -89,27 +39,28 @@ public class TestVehicleLocationInferenceServiceImpl {
     CsvEntityMappingBean record = helper.addEntity(NycTestLocationRecord.class);
     record.setAutoGenerateSchema(false);
     reader.setEntitySchemaFactory(factory);
-    record.addAdditionalFieldMapping(new AbstractFieldMapping(NycTestLocationRecord.class, "", "", true) {
-      
+    record.addAdditionalFieldMapping(new AbstractFieldMapping(
+        NycTestLocationRecord.class, "", "", true) {
+
       @Override
       public void translateFromObjectToCSV(CsvEntityContext context,
           BeanWrapper object, Map<String, Object> csvValues)
           throws CsvEntityException {
         /* don't bother */
       }
-      
+
       @Override
       public void translateFromCSVToObject(CsvEntityContext context,
           Map<String, Object> csvValues, BeanWrapper object)
           throws CsvEntityException {
-        
+
         NycTestLocationRecord record = object.getWrappedInstance(NycTestLocationRecord.class);
         record.setDsc(csvValues.get("dsc").toString());
         record.setTimestamp(csvValues.get("dt").toString());
         record.setVehicleId(csvValues.get("vid").toString());
         record.setLat(Double.parseDouble(csvValues.get("lat").toString()));
         record.setLon(Double.parseDouble(csvValues.get("lon").toString()));
-      } 
+      }
     });
 
     reader.addEntityHandler(new EntityHandler() {
@@ -129,7 +80,7 @@ public class TestVehicleLocationInferenceServiceImpl {
         service.handleVehicleLocation(location);
       }
     });
-    
+
     InputStream in = getClass().getResourceAsStream("ivn-dsc.csv");
     assertNotNull(in);
     reader.readEntities(NycTestLocationRecord.class, in);
@@ -137,7 +88,7 @@ public class TestVehicleLocationInferenceServiceImpl {
     /* wait a max of two seconds for processing to finish */
     VehicleLocationRecord lastLocation = null;
     for (int i = 0; i < 20; ++i) {
-      Thread.sleep(100); //wait for processing to finish (but not very long)
+      Thread.sleep(100); // wait for processing to finish (but not very long)
       List<VehicleLocationRecord> records = service.getLatestProcessedVehicleLocationRecords();
       assertEquals(1, records.size());
       lastLocation = records.get(0);
