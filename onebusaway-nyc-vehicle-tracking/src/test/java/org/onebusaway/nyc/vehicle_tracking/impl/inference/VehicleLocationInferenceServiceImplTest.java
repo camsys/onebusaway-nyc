cbfdd4678d76547a7ccf035f43072b5e7843871b
novalis@novalis.org <novalis@novalis.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-11 19:11:14 +0000

    fix test
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2544 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/VehicleLocationInferenceServiceImplTest.java
+++ onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/VehicleLocationInferenceServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,15 +22,34 @@ import org.onebusaway.gtfs.csv.schema.EntitySchemaFactoryHelper;
 import org.onebusaway.gtfs.csv.schema.beans.CsvEntityMappingBean;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.nyc.vehicle_tracking.services.VehicleLocationInferenceRecord;
+import org.onebusaway.realtime.api.VehicleLocationListener;
 import org.onebusaway.realtime.api.VehicleLocationRecord;
 
 public class VehicleLocationInferenceServiceImplTest {
+  class MockVehicleLocationListener implements VehicleLocationListener {
+    public ArrayList<VehicleLocationRecord> storedRecords;
 
+    MockVehicleLocationListener() {
+      storedRecords = new ArrayList<VehicleLocationRecord>();
+    }
+    @Override
+    public void handleVehicleLocationRecord(VehicleLocationRecord record) {
+      storedRecords.add(record);
+    }
+
+    @Override
+    public void handleVehicleLocationRecords(List<VehicleLocationRecord> records) {
+      storedRecords.addAll(records);
+    }
+    
+  }
   @Test
   public void testInference() throws CsvEntityIOException, IOException,
       InterruptedException {
     final VehicleLocationInferenceServiceImpl service;
     service = new VehicleLocationInferenceServiceImpl();
+    MockVehicleLocationListener mockVehicleLocationListener = new MockVehicleLocationListener();
+    service.setVehicleLocationListener(mockVehicleLocationListener );
     service.start();
     CsvEntityReader reader = new CsvEntityReader();
 
@@ -89,9 +109,8 @@ public class VehicleLocationInferenceServiceImplTest {
     VehicleLocationRecord lastLocation = null;
     for (int i = 0; i < 20; ++i) {
       Thread.sleep(100); // wait for processing to finish (but not very long)
-      List<VehicleLocationRecord> records = service.getLatestProcessedVehicleLocationRecords();
-      assertEquals(1, records.size());
-      lastLocation = records.get(0);
+      List<VehicleLocationRecord> records = mockVehicleLocationListener.storedRecords;
+      lastLocation = records.get(records.size() - 1);
       if (Math.abs(40.717632 - lastLocation.getCurrentLocationLat()) < 0.0000001) {
         break;
       }
