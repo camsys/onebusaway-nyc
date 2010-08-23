85d9a08fd83fcf55c036f1caa2281ddc5607a896
rmarianski <rmarianski@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-23 22:35:55 +0000

    update integration test to match new ui
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2593 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-integration-tests/src/test/java/org/onebusaway/nyc/integration_tests/nyc_webapp/HelloWorldIntegrationTest.java
+++ onebusaway-nyc-integration-tests/src/test/java/org/onebusaway/nyc/integration_tests/nyc_webapp/HelloWorldIntegrationTest.java
@@ -11,13 +11,15 @@ public class HelloWorldIntegrationTest extends NycWebappTestSupport {
 
     // Open the webpage in selenium
     open("/index.action");
-    
-    String expectedText = "Hello one bus away NY!";
 
-    // Verify that text is present
-    assertTrue(isTextPresent(expectedText));
+    // verify that the header is coming through
+    String header = "A City Transit Agency";
+    assertTrue(isTextPresent(header));
+    assertEquals(header, getText("xpath=//div[@id='header']/h1"));
 
-    // Verify that text is present at a particular DOM location
-    assertEquals(expectedText, getText("xpath=html/body/h1"));
+    // verify that the default search stub text appears on the page
+    String searchText = "You haven't added any routes to the map yet.";
+    assertTrue(isTextPresent(searchText));
+    assertEquals(searchText, getText("xpath=//p[@id='no-routes-displayed-message']"));
   }
 }
