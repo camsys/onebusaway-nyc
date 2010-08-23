75a3641050f1ec8a6047f5fcc0aa48ac04da32f2
rmarianski <rmarianski@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-08-23 22:38:59 +0000

    name index page integration test more appropriately
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2594 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-integration-tests/src/test/java/org/onebusaway/nyc/integration_tests/nyc_webapp/NycWebappIndexPageIntegrationTest.java
+++ onebusaway-nyc-integration-tests/src/test/java/org/onebusaway/nyc/integration_tests/nyc_webapp/NycWebappIndexPageIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class HelloWorldIntegrationTest extends NycWebappTestSupport {
+public class NycWebappIndexPageIntegrationTest extends NycWebappTestSupport {
   @Test
   public void test() {
 
