a890ee40eb517dc60df09fb3bdc6dca464f737c8
bdferris@onebusaway.org <bdferris@onebusaway.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-09-17 16:54:24 +0000

    Sensor model unit test
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2725 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/SensorModelImplTest.java
+++ onebusaway-nyc-vehicle-tracking/src/test/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/SensorModelImplTest.java
@@ -1,6 +1,10 @@
 package org.onebusaway.nyc.vehicle_tracking.impl.inference;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.onebusaway.nyc.vehicle_tracking.impl.particlefilter.Gaussian;
 import org.onebusaway.nyc.vehicle_tracking.impl.particlefilter.Particle;
 import org.onebusaway.nyc.vehicle_tracking.model.NycVehicleLocationRecord;
 import org.onebusaway.transit_data_federation.impl.tripplanner.offline.BlockEntryImpl;
@@ -8,7 +12,6 @@ import org.onebusaway.transit_data_federation.services.blocks.BlockInstance;
 import org.onebusaway.transit_data_federation.services.blocks.ScheduledBlockLocation;
 import org.onebusaway.transit_data_federation.testing.DateSupport;
 import org.onebusaway.transit_data_federation.testing.MockEntryFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class SensorModelImplTest {
 
@@ -19,30 +22,35 @@ public class SensorModelImplTest {
     _model = new SensorModelImpl();
   }
 
-  @Autowired
-  public void test() {
-    long serviceDate = DateSupport.time("2010-09-16 00:00");
-    long t = DateSupport.time("2010-09-16 10:00");
+  @Test
+  public void testComputeScheduleDeviationPropability() {
 
-    
-    EdgeState edgeState = new EdgeState(null);
+    long serviceDate = DateSupport.time("2010-09-16 00:00");
+    long now = DateSupport.time("2010-09-16 10:00");
+
+    EdgeState edgeState = new EdgeState(null,null,0.0);
 
     BlockEntryImpl blockEntry = MockEntryFactory.block("blockA");
     BlockInstance blockInstance = new BlockInstance(blockEntry, serviceDate);
-    ScheduledBlockLocation blockLocation = new ScheduledBlockLocation();
 
+    ScheduledBlockLocation blockLocation = new ScheduledBlockLocation();
+    blockLocation.setScheduledTime(DateSupport.hourAndMinutesToSec(9, 50));
     BlockState blockState = new BlockState(blockInstance, blockLocation, "1234");
 
     VehicleState vehicleState = new VehicleState(edgeState, blockState);
 
-    Particle p = new Particle(t);
+    Particle p = new Particle(now);
     p.setData(vehicleState);
-    
+
     NycVehicleLocationRecord record = new NycVehicleLocationRecord();
     record.setLatitude(47.0);
+    record.setTime(now);
     
     Observation obs = new Observation(record, null);
 
-    double v = _model.computeScheduleDeviationProbability(p, obs);
+    Gaussian g = new Gaussian(0, 15 * 60);
+    double expectedP = g.getProbability(10 * 60);
+    double actualP = _model.computeScheduleDeviationProbability(p, obs);
+    assertEquals(expectedP, actualP, 0.0);
   }
 }
