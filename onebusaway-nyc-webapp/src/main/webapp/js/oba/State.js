9b4f8835af4bc8e017d11b9f9b31b829b2a01cac
rmarianski <rmarianski@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-09-19 19:55:51 +0000

    don't saved the currently displayed routes since it's not split up by trip and it'll be missing the trip headsigns
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@2850 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/State.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/State.js
@@ -10,72 +10,6 @@ OBA.State = function(map, routeMap, makeRouteFn) {
             state[k] = v;
         } 
     }
-    
-    // fetch one route at a time
-    function fetchRoutes(routeDirectionId, remainingRouteDirectionIds, displayedRoutes, displayedRoutesList) {
-        var routeId = routeDirectionId[0];
-        var directionId = routeDirectionId[1];
-        var url = OBA.Config.routeShapeUrl + "/" + routeId + ".json";
-        jQuery.ajax({
-            beforeSend: function(xhr) {
-                displayedRoutes.addClass("loading");                
-            },
-            complete: function(xhr, s) {
-                displayedRoutes.removeClass("loading");
-            },
-            success: function(json, s, xhr) {
-                
-                var shape;
-                try {
-                    shape = json.data.entry.polylines;
-                } catch (typeError) {
-                    OBA.Util.log("invalid route response from server");
-                    OBA.Util.log(json);
-                    return;
-                }
-
-                    routeMap.addRoute(routeId, directionId, shape);
-                    
-                    var jsonRouteResponse;
-                    try {
-                        jsonRouteResponse = json.data.references.routes[0];
-                    } catch (typeError) {
-                        OBA.Util.log("invalid route response from server");
-                        OBA.Util.log(json);
-                        return;
-                    }
-
-                    // adapt the data returned from server to match what the route
-                    // generator function expects
-                    var routeData = {routeId: routeId,
-                                     directionId: directionId,
-                                     tripHeadsign: jsonRouteResponse.shortName + " " + jsonRouteResponse.longName,
-                                     //serviceNotice: null,
-                                     name: jsonRouteResponse.shortName,
-                                     description: jsonRouteResponse.longName};                    
-
-                    var clonedDiv = makeRouteFn(routeData);
-                    var clonedControlLink = clonedDiv.find(".addToMap");
-
-                    clonedDiv.attr("id", "displayedroute-" + routeId);
-
-                    // update the control link class to alter the event fired 
-                    var clonedControlLink = clonedDiv.find(".addToMap");
-                    clonedControlLink.removeClass("addToMap");
-                    clonedControlLink.addClass("removeFromMap");
-                    clonedControlLink.html("Remove from map");
-
-                    displayedRoutesList.append(jQuery("<li></li>").append(clonedDiv));
-                    
-                    if (remainingRouteDirectionIds.length > 0)
-                        fetchRoutes(remainingRouteDirectionIds[0], remainingRouteDirectionIds.slice(1),
-                                    displayedRoutes, displayedRoutesList);
-            },
-            dataType: "json",
-            data: {version: 2, key: OBA.Config.apiKey},
-            url: url
-        });
-    }
 
     function loadState(hash) {
 		if(currentHash === hash || hash === null)
@@ -95,47 +29,6 @@ OBA.State = function(map, routeMap, makeRouteFn) {
 			map.setZoom(parseInt(state.z));
 		}
 
-		// displayed route state
-		if(typeof state.r !== 'undefined' && state.r != "") {
-		    var encodedRouteDirectionIds = state.r.split("^");
-		    
-		    var routeDirectionIds = [];
-		    jQuery.each(encodedRouteDirectionIds, function(_, routeDirectionId) {
-		        var routeDirectionPair = routeDirectionId.split("!");
-		        routeDirectionIds.push(routeDirectionPair);
-		    });
-
-			var displayedRoutes = jQuery("#displayed-routes");
-			var displayedRoutesList = jQuery("#displayed-routes-list");
-
-	        if (routeDirectionIds.length > 0) {
-                displayedRoutesList.empty();
-
-                routeMap.removeAllRoutes();
-//                jQuery.each(routeMap.getRoutes(), function(_, routeId) {
-//                    routeMap.removeRoute(routeId);
-//                });
-                
-	            fetchRoutes(routeDirectionIds[0], routeDirectionIds.slice(1), displayedRoutes, displayedRoutesList);
-	            
-	            jQuery("#no-routes-displayed-message").hide();
-	            jQuery("#n-displayed-routes").text(routeDirectionIds.length);
-	        }
-		} else {	
-			jQuery("#n-displayed-routes").text("0");
-			jQuery("#displayed-routes-list").empty();
-			jQuery("#no-routes-displayed-message").show();
-
-//			var r = routeMap.getRoutes();
-//
-//			for(var i = 0; i < r.length; i++) {
-//				var route = r[i];
-//	
-//				routeMap.removeRoute(route);
-//			}
-			routeMap.removeAllRoutes();
-		}
-
 		haveRestored = true;
     }
    
@@ -147,11 +40,6 @@ OBA.State = function(map, routeMap, makeRouteFn) {
         setState("z", map.getZoom());
         setState("lng", map.getCenter().lng());
         setState("lat", map.getCenter().lat());
-        var routeDirections = routeMap.getRoutes();
-        var serializedRouteDirections = jQuery.map(routeDirections, function(routeDirection) {
-            return routeDirection[0] + "!" + routeDirection[1];
-        });
-		setState("r", serializedRouteDirections.join("^"));
 
         serialize();
     }
