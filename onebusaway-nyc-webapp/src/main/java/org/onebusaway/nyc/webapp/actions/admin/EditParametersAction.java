fcbe940d12a6688f44cd1f788167cf4703a0dec8
novalis@novalis.org <novalis@novalis.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-10-25 21:57:33 +0000

    first stab at admin interface (will not work due to security)
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3132 638cd66d-8850-0410-a654-37fb4b726c3a

---
+++ onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/admin/EditParametersAction.java
@@ -0,0 +1,101 @@
+package org.onebusaway.nyc.webapp.actions.admin;
+
+import org.onebusaway.nyc.vehicle_tracking.services.VehicleTrackingConfigurationService;
+import org.apache.struts2.interceptor.validation.SkipValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class EditParametersAction extends ActionSupport {
+
+  private static final long serialVersionUID = 1L;
+
+  private Integer noProgressTimeout;
+  private Double offRouteDistance;
+  private Integer staleDataTimeout;
+  private Integer staleDataGhostTimeout;
+
+  @Autowired
+  private VehicleTrackingConfigurationService _configService;
+
+  @Override
+  @SkipValidation
+  public String execute() {
+    noProgressTimeout = _configService.getVehicleStalledTimeThreshold();
+    offRouteDistance = _configService.getVehicleOffRouteDistanceThreshold();
+    staleDataTimeout = 300; // TODO
+    staleDataGhostTimeout = 120; // TODO
+    return SUCCESS;
+  }
+
+
+  // @Validations(requiredStrings = {@RequiredStringValidator(fieldName =
+  // "noProgressTimeout", message = "Hey!")})
+  public String submit() {
+
+    boolean failed = false;
+    if (noProgressTimeout == null) {
+      noProgressTimeout = _configService.getVehicleStalledTimeThreshold();
+      failed = true;
+    } else {
+      _configService.setVehicleStalledTimeThreshold(noProgressTimeout);
+    }
+
+    if (offRouteDistance == null) {
+      offRouteDistance = _configService.getVehicleOffRouteDistanceThreshold();
+      failed = true;
+    } else {
+      _configService.setVehicleOffRouteDistanceThreshold(offRouteDistance);
+    }
+
+    if (staleDataTimeout == null) {
+      // staleDataTimeout = _configService.get
+      staleDataTimeout = 300;
+      failed = true;
+    }
+    if (staleDataGhostTimeout == null) {
+      // staleDataTimeout = _configService.get
+      staleDataTimeout = 120;
+      failed = true;
+    }
+    if (failed) {
+      return INPUT;
+    }
+
+    return SUCCESS;
+  }
+
+
+  public void setNoProgressTimeout(int noProgressTimeout) {
+    this.noProgressTimeout = noProgressTimeout;
+  }
+
+  public String getNoProgressTimeout() {
+    return "" + noProgressTimeout;
+  }
+
+  public void setOffRouteDistance(double offRouteDistance) {
+    this.offRouteDistance = offRouteDistance;
+  }
+
+  public double getOffRouteDistance() {
+    return offRouteDistance;
+  }
+
+  public void setStaleDataTimeout(int staleDataTimeout) {
+    this.staleDataTimeout = staleDataTimeout;
+  }
+
+  public int getStaleDataTimeout() {
+    return staleDataTimeout;
+  }
+
+  public void setStaleDataGhostTimeout(int staleDataGhostTimeout) {
+    this.staleDataGhostTimeout = staleDataGhostTimeout;
+  }
+
+  public int getStaleDataGhostTimeout() {
+    return staleDataGhostTimeout;
+  }
+
+}
