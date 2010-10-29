e66095d71cb44dcf0b10d2f5395344fa10e836ee
jmaki@openplans.org <jmaki@openplans.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-10-29 22:32:13 +0000

    + OBA wiki integration
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3167 638cd66d-8850-0410-a654-37fb4b726c3a

---
+++ onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/wiki/HelpAction.java
@@ -0,0 +1,9 @@
+package org.onebusaway.nyc.webapp.actions.wiki;
+
+import org.onebusaway.nyc.webapp.actions.OneBusAwayNYCActionSupport;
+
+public class HelpAction extends OneBusAwayNYCActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+}
