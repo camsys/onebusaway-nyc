a1d92f7a0705f3f9bfb68bab73805cf7605f7984
rmarianski <rmarianski@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-11-11 00:00:25 +0000

    add in form logic to save disabled vehicles
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3262 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/admin/VehiclesAction.java
+++ onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/admin/VehiclesAction.java
@@ -1,13 +1,22 @@
 package org.onebusaway.nyc.webapp.actions.admin;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.onebusaway.geospatial.model.CoordinatePoint;
 import org.onebusaway.nyc.presentation.impl.WebappIdParser;
+import org.onebusaway.nyc.transit_data.model.NycVehicleStatusBean;
+import org.onebusaway.nyc.transit_data.services.VehicleTrackingManagementService;
 import org.onebusaway.nyc.webapp.actions.OneBusAwayNYCActionSupport;
 import org.onebusaway.realtime.api.EVehiclePhase;
 import org.onebusaway.transit_data.model.ListBean;
@@ -16,6 +25,8 @@ import org.onebusaway.transit_data.model.trips.TripBean;
 import org.onebusaway.transit_data.services.TransitDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Results( {@Result(type = "redirectAction", name = "redirect", params = {
+    "namespace", "/admin", "actionName", "vehicles"})})
 public class VehiclesAction extends OneBusAwayNYCActionSupport implements ServletRequestAware {
 
   private static final long serialVersionUID = 1L;
@@ -24,11 +35,13 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
 
   @Autowired
   private TransitDataService transitService;
+  
+  @Autowired
+  private VehicleTrackingManagementService vehicleTrackingManagementService;
 
   private List<VehicleBag> vehicles = new ArrayList<VehicleBag>();
 
   private HttpServletRequest request;
-
 
   @Override
   public void setServletRequest(HttpServletRequest request) {
@@ -40,20 +53,55 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
     ListBean<VehicleStatusBean> vehiclesForAgencyListBean = transitService.getAllVehiclesForAgency("MTA NYCT", System.currentTimeMillis());
     List<VehicleStatusBean> vehicleStatusBeans = vehiclesForAgencyListBean.getList();
     
-    String method = request.getMethod().toUpperCase();
-    if (method.equals("POST")) {
-//      Enumeration parameterNames = request.getParameterNames();
-//      while (parameterNames.hasMoreElements()) {
-//        String key = parameterNames.nextElement().toString();
-//        if (key.startsWith("disable_")) {
-//          String vehicleId = key.substring("disable".length());
-//          // TODO fill in disabling logic
-//        }
-//      }
+    // first get a model of all the vehicle statuses key'ed off vehicle id for easy lookup
+    List<NycVehicleStatusBean> allVehicleStatuses = vehicleTrackingManagementService.getAllVehicleStatuses();
+    Map<String, NycVehicleStatusBean> vehicleStatusMap = new HashMap<String, NycVehicleStatusBean>();
+    for (NycVehicleStatusBean nycVehicleStatusBean : allVehicleStatuses) {
+      String vehicleId = nycVehicleStatusBean.getVehicleId();
+      vehicleStatusMap.put(vehicleId, nycVehicleStatusBean);
     }
 
+    String method = request.getMethod().toUpperCase();
+    if (method.equals("POST")) {
+      // keep track of vehicles that have been disabled so we can enable the others
+      Set<String> disabledVehicles = new HashSet<String>();
+
+      Enumeration<?> parameterNames = request.getParameterNames();
+      while (parameterNames.hasMoreElements()) {
+        String key = parameterNames.nextElement().toString();
+        if (key.startsWith("disable_")) {
+          String vehicleId = key.substring("disable_".length());
+          NycVehicleStatusBean nycVehicleStatusBean = vehicleStatusMap.get(vehicleId);
+          if (nycVehicleStatusBean == null) {
+            vehicleTrackingManagementService.setVehicleStatus(vehicleId, false);            
+          } else {
+            // no need to call disable on it if it's already disabled
+            boolean isDisabled = !nycVehicleStatusBean.isEnabled();
+            if (!isDisabled)
+              vehicleTrackingManagementService.setVehicleStatus(vehicleId, false);
+          }
+          disabledVehicles.add(vehicleId);
+        }
+      }
+
+      // enable all the vehicles that haven't been explicitly disabled from the interface
+      for (NycVehicleStatusBean nycVehicleStatusBean : allVehicleStatuses) {
+        String vehicleId = nycVehicleStatusBean.getVehicleId();
+        if (!disabledVehicles.contains(vehicleId))
+            vehicleTrackingManagementService.setVehicleStatus(vehicleId, true);
+      }
+      
+      return "redirect";
+    }
+    
     for (VehicleStatusBean vehicleStatusBean : vehicleStatusBeans) {
-      VehicleBag vehicleBag = new VehicleBag(vehicleStatusBean);
+      String vehicleId = vehicleStatusBean.getVehicleId();
+      NycVehicleStatusBean nycVehicleStatusBean = vehicleStatusMap.get(vehicleId);
+      boolean isDisabled = false;
+      if (nycVehicleStatusBean != null) {
+        isDisabled = !nycVehicleStatusBean.isEnabled();
+      }
+      VehicleBag vehicleBag = new VehicleBag(vehicleStatusBean, isDisabled);
       vehicles.add(vehicleBag);
     }
     return SUCCESS;
@@ -66,9 +114,11 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
   // vehicle data bag suitable for use in vehicles jsp
   private static class VehicleBag {
     private VehicleStatusBean vehicleStatusBean;
+    private boolean vehicleDisabled;
 
-    public VehicleBag(VehicleStatusBean vehicleStatusBean) {
+    public VehicleBag(VehicleStatusBean vehicleStatusBean, boolean isDisabled) {
       this.vehicleStatusBean = vehicleStatusBean;
+      this.vehicleDisabled = isDisabled;
     }
     
     @SuppressWarnings("unused")
@@ -94,7 +144,7 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
         return "status red";
       if (timeDiff > orangeMillisThreshold)
         return "status orange";
-      if (!status.equals(EVehiclePhase.IN_PROGRESS.toString()))
+      if (status == null || !status.equals(EVehiclePhase.IN_PROGRESS.toString()))
         return "status orange";
       return "status normal";
     }
@@ -127,7 +177,7 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
       if (trip == null)
         return "No Trip";
       String status = vehicleStatusBean.getStatus();
-      if (status.equals(EVehiclePhase.IN_PROGRESS.toString()))
+      if (status != null && status.equals(EVehiclePhase.IN_PROGRESS.toString()))
         return "Normal";
       return "Unknown";
     }
@@ -153,9 +203,8 @@ public class VehiclesAction extends OneBusAwayNYCActionSupport implements Servle
     }
 
     @SuppressWarnings("unused")
-    public boolean isDisabled() {
-      // TODO fill in
-      return false;
+    public String isDisabled() {
+      return Boolean.valueOf(this.vehicleDisabled).toString();
     }
   }
 }
