400cd3e0ad28c9e4d34d21f12c6d9e866698d5e9
novalis@novalis.org <novalis@novalis.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-11-12 19:01:40 +0000

    add siri replayer
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3286 638cd66d-8850-0410-a654-37fb4b726c3a

---
+++ onebusaway-nyc-vehicle-tracking-webapp/src/main/python/replay-siri.py
@@ -0,0 +1,32 @@
+from sqlobject import *
+from time import time, sleep
+from httplib2 import Http
+
+sqlhub.processConnection = connectionForURI('postgresql://onebusaway:onebusaway@localhost/onebusaway')
+
+targetServer = "http://novalis.org/cgi/env.cgi"
+
+class LocationRecord(SQLObject):
+    class sqlmeta:
+        table = "oba_nyc_raw_location"
+        style = MixedCaseStyle()
+    timeReceived = IntCol()
+    rawData = StringCol()
+   
+
+lastCheck = time() * 1000
+
+while 1:
+    sleep(1)
+    for row in LocationRecord.select(LocationRecord.q.timeReceived > lastCheck).orderBy("timeReceived"):
+        print "sending row"
+        h = Http()
+        resp, content = h.request(targetServer, 
+                                  "POST", body=row.rawData)
+        if not resp['status'] == '200':
+            print resp, content
+            print rawdata
+            print
+
+        if lastCheck < row.timeReceived:
+            lastCheck = row.timeReceived
