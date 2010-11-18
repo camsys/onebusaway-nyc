a851666f53fb60430219c859e52401d1c7b07ff5
jmaki@openplans.org <jmaki@openplans.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-11-18 15:44:45 +0000

    + IE6 tweaks + Admin interface: inline map, a la Frumin's "hacked" admin interface
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3423 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/VehiclesAdmin.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/VehiclesAdmin.js
@@ -16,15 +16,8 @@
 
 var OBA = window.OBA || {};
 
-// state to keep track of vehicle id -> latlng
-var latlngs = {};
-
-function _get_vehicle_table() {
-    return jQuery('#vehicle-table');
-}
-
 function addTableSortBehaviors() {
-    var table = _get_vehicle_table();
+    var table = jQuery('#vehicle-table');
     // for reverse sorting
     var lastSortIndex = -1;
     table.find('th a').each(function(i, a) {
@@ -51,97 +44,38 @@ function addTableSortBehaviors() {
                 lastSortIndex = i;
             }
             table.append(rows);
-            // when we re-add the rows we lose behavior
-            addShowMapBehavior();
         });
     });
 }
 
-function _createMapOverlay() {
-    var div = jQuery('<div id="map-overlay"><a href="#">Close</a><div id="google-map"></div></div>');
-    return div;
-}
-
-function _removePreviousMaps() {
-    jQuery("#map-overlay").remove();
-}
-
-function _addGoogleMap(lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-      zoom: 16,
-      center: latlng,
-      mapTypeControl: false,
-      navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
-    var marker = new google.maps.Marker({
-        position: latlng, 
-        map: map
-    });
-}
-
-function _makeMiniMap(lat, lng, x, y) {
-    var popup = _createMapOverlay(lat, lng);
-    popup.css({top: y, left: x});
-    popup.appendTo(jQuery("body"));
-    popup.find('a').click(function() {
-        popup.remove();
-        return false;
-    });
-    _addGoogleMap(lat, lng);
-}
-
-function _vehicleIdForCell(elt) {
-    // for a given cell (jquery elt), find the vehicleId for that row
-    var td = elt;
-    var node = elt.get(0);
-    if (node.nodeName.toUpperCase !== "TD") {
-        td = elt.parents("td").first();
-    }
-    if (!td.hasClass("vehicleId")) {
-        td = td.siblings(".vehicleId");
-    }
-    var vehicleId = td.text();
-    return vehicleId;
-}
-
-function tweakLatLngsAndPrepareData() {
-    var table = _get_vehicle_table();
-    table.find('.map-location').each(function(i, span) {
-        var span = jQuery(span);
-        var a = span.siblings('a');
-        var location = span.text();
-        var fields = location.split(",");
-        var lat = fields[0], lng = fields[1];
-        var vehicleId = _vehicleIdForCell(span);
-        latlngs[vehicleId] = {lat: lat, lng: lng};
-        var trimlat = parseFloat(lat).toPrecision("7");
-        var trimlng = parseFloat(lng).toPrecision("7");
-        span.text(trimlat + "," + trimlng);
-    });
-}
-
-function addShowMapBehavior() {
-    var table = _get_vehicle_table();
-    table.find('.map-link').click(function(e) {
-        e.preventDefault();
-        _removePreviousMaps();
-        var td = jQuery(this).parent();
-        var tdPosition = td.offset();
-        var x = tdPosition.left - 200 - 50;
-        var y = tdPosition.top - 100;
-        var vehicleId = _vehicleIdForCell(jQuery(this));
-        var latlng = latlngs[vehicleId];
-        _makeMiniMap(latlng.lat, latlng.lng, x, y);
-    });
+function createMaps() {
+	jQuery(".map-location").each(function(_, elRaw) {
+		var el = jQuery(elRaw);
+		
+		var location = el.html();
+		var location_v = location.split(",");
+		var lat = location_v[0];
+		var lng = location_v[1];
+		var latlng = new google.maps.LatLng(lat, lng);
+		var mapOptions = {
+		      zoom: 15,
+		      center: latlng,
+		      mapTypeControl: false,
+		      disableDefaultUI: true,
+		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+			
+		var map = new google.maps.Map(elRaw, mapOptions);
+		var marker = new google.maps.Marker({
+		      position: latlng, 
+		      map: map
+		});
+	});
 }
 
 jQuery(document).ready(function() {
     addTableSortBehaviors();
-    tweakLatLngsAndPrepareData();
-    addShowMapBehavior();
+    createMaps();
     
     // refresh every 1m
     setTimeout(function() {
