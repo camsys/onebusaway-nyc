ad2c6324801313a5eb01ee52af15420c36e81e25
jmaki@openplans.org <jmaki@openplans.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2011-01-03 18:39:49 +0000

    + AJAX update of admin screen
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3724 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/VehiclesAdmin.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/VehiclesAdmin.js
@@ -15,125 +15,133 @@
 (function() {
 
 var OBA = window.OBA || {};
+	var lastSortIndex = -1;
+	var reverseSort = false;
+	
+	var markers = {};
+	
+	function sortTableRows(rows, sortIndex, reverse) { 
+        var keyFn = function(row) {
+            var key = jQuery(row).children().slice(sortIndex, sortIndex+1).text();
+            return key;
+        };
+
+        rows = jQuery.makeArray(rows);
+        rows.sort(function(a, b) {
+            var x = keyFn(a);
+            var y = keyFn(b);
+            if (x < y) { return -1; }
+            else if (x > y) { return 1; }
+            else { return 0; }
+        });
+
+        if(reverse === true) {
+            rows.reverse();
+        }
+
+        return rows;
+	}
+
 	function addTableSortBehaviors() {
 	    var table = jQuery('table');
-	    // for reverse sorting
-	    var lastSortIndex = -1;
-	
+
 	    table.find('th a').each(function(i, a) {
-	        var keyFn = function(row) {
-	            var key = jQuery(row).children().slice(i, i+1).text();
-	            return key;
-	        };
-	    
 	        jQuery(a).click(function(e) {
 	            e.preventDefault();
 
-	            var rows = table.find('tr').not(":first");
-	            
-	            rows.remove();
-	            rows = jQuery.makeArray(rows);
-	            rows.sort(function(a, b) {
-	                var x = keyFn(a);
-	                var y = keyFn(b);
-	                if (x < y) { return -1; }
-	                else if (x > y) { return 1; }
-	                else { return 0; }
-	            });
-	            
-	            if (lastSortIndex === i) {
-	                rows.reverse();
-	                lastSortIndex = -1;
-	            } else {
-	                lastSortIndex = i;
+	            if(lastSortIndex === i) {
+	            	reverseSort = !reverseSort;
 	            }
+
+	            var oldRows = table.find('tr').not(":first");
+	            var newRows = sortTableRows(oldRows, i, reverseSort);	      
+
+	            lastSortIndex = i;
 	            
-	            table.append(rows);
+	            oldRows.remove();
+	            table.append(newRows);			            
 	        });
 	    });
 	}
 	
-	function createMaps() {
-		jQuery(".map").each(function(_, elRaw) {
-			var el = jQuery(elRaw);
+	function createMap(elRaw) {
+		var el = jQuery(elRaw);
 
-			var contents = el.html();
-			if(contents === null) {
-				return;
-			}
-			
-			var location_r = contents.match(/Location\: ([0-9|.|\-| |,]*)/i);
-			var orientation_r = contents.match(/Orientation\: ([0-9]*)/i);
-			if(location_r === null || orientation_r === null 
-				|| location_r.length !== 2 || location_r[1] === ""
-				|| orientation_r.length !== 2) {
-				return;
-			}
-			
-			var location = location_r[1].split(",");			
-
-			if(location === null || location.length !== 2) {
-				return;
-			}
-			
-			var lat = location[0];
-			var lng = location[1];
-
-			if(lat === null || lng === null) {
-				return;
-			}
-			
-			var orientation = Math.floor(orientation_r[1] / 5) * 5;
-	
-			if(orientation === null || orientation === "" || orientation === 0) {
-				orientation = "unknown";
-			}
-			
-			var mapDiv = jQuery("<div></div>").addClass("map-location");
-			var mapWrapper = jQuery("<div></div>").addClass("map-location-wrapper").append(mapDiv);
-
-			el.html("");
-			el.append(mapWrapper);
-	
-			var latlng = new google.maps.LatLng(lat, lng);
-			
-			var mapOptions = {
-			      zoom: 15,
-			      center: latlng,
-			      mapTypeControl: false,
-			      streetViewControl: false,
-				  navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
-				  mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-				
-			var map = new google.maps.Map(mapDiv.get(0), mapOptions);
+		var contents = el.html();
+		if(contents === null) {
+			return null;
+		}
 		
-			var icon = new google.maps.MarkerImage(OBA.Config.vehicleIconFilePrefix + '-' + orientation + '.' + OBA.Config.vehicleIconFileType,
-					OBA.Config.vehicleIconSize,
-					new google.maps.Point(0,0),
-					OBA.Config.vehicleIconCenter);
-	    	
-			var marker = new google.maps.Marker({
-			      position: latlng, 
-				  icon: icon,
-			      map: map,
-			      clickable: false
-			});
-	
-			// (FIXME?) Google maps seems to always take into account marker padding with the assumption of a 
-			// marker that is shaped like its default (i.e. taller than wide). Because of this, we have to adjust 
-			// the map center to make up for this false assumption in our case.
-			map.panBy(0, 18);
-		});
+		var location_r = contents.match(/Location\: ([0-9|.|\-| |,]*)/i);
+		var orientation_r = contents.match(/Orientation\: ([0-9]*)/i);
+		if(location_r === null || orientation_r === null 
+			|| location_r.length !== 2 || location_r[1] === ""
+			|| orientation_r.length !== 2) {
+			return null;
+		}
+		
+		var location = location_r[1].split(",");			
+		if(location === null || location.length !== 2) {
+			return null;
+		}
+		
+		var lat = location[0];
+		var lng = location[1];
+		if(lat === null || lng === null) {
+			return null;
+		}
+		
+		var orientation = Math.floor(orientation_r[1] / 5) * 5;
+		if(orientation === null || orientation === "" || orientation === 0) {
+			orientation = "unknown";
+		}
+
+		var mapDivWrapper = jQuery("<div></div>")
+							.addClass("map-location-wrapper");
+		
+		var iconUrl = "http://dev.oba.openplans.org/" + OBA.Config.vehicleIconFilePrefix + '-' + orientation + '.' + OBA.Config.vehicleIconFileType;
+		var image = jQuery("<img></img>")
+						.addClass("map-location")
+						.appendTo(mapDivWrapper)
+						.attr("src", "http://maps.google.com/maps/api/staticmap?size=150x160&markers=shadow:false|icon:" + iconUrl + "|" + lat + "," + lng + "&zoom=15&sensor=false");
+
+		el.html("");
+		el.append(mapDivWrapper);
 	}
 	
 	jQuery(document).ready(function() {
 	    addTableSortBehaviors();
-	    createMaps();
+	    
+		jQuery("td.map").each(function(_, el) {
+			createMap(el);
+		});
 
-	    // refresh every 1m
-	    setTimeout(function() {
-	    	window.location.href = window.location.href;
-	    }, 60 * 1000);
+	    // refresh every 30s
+	    var refreshFunction = function() {
+	    	jQuery.ajax({
+                url: window.location.href,
+                success: function(data) { 
+	                 var table = jQuery("table");
+	
+	                 var oldTableRows = table.find('tr').not(":first");
+	               	 var newTableRows = sortTableRows(
+	               			 	jQuery(data).find("table").find('tr').not(":first"),
+	               			 	lastSortIndex,
+	               			 	reverseSort);
+	 	 
+	               	 oldTableRows.remove();
+	               	 table.append(newTableRows);
+	               	 
+	               	 jQuery("td.map").each(function(_, el) {
+	        			createMap(el);
+	               	 });
+                }
+            });
+		    
+		    setTimeout(refreshFunction, 30 * 1000);
+	    };
+	    
+	    // kick off the updates...
+	    setTimeout(refreshFunction, 30 * 1000);
 	});
 })();
