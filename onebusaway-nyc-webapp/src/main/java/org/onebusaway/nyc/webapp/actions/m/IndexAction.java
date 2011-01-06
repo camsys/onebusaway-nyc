decfea03c30fce51219455d097f753e2b5364a15
jmaki@openplans.org <jmaki@openplans.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2011-01-06 01:55:55 +0000

    + "No results found" mobile messaging
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@3736 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/m/IndexAction.java
+++ onebusaway-nyc-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/m/IndexAction.java
@@ -33,6 +33,10 @@ public class IndexAction extends OneBusAwayNYCActionSupport {
     return searchResults;
   }
 
+  public boolean getQueryIsEmpty() {
+	  return q.isEmpty();
+  }
+  
   public String getQ() {
     return q;
   }
