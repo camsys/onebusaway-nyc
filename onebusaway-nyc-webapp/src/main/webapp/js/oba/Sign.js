b8ba539d5be67d080dad533f9a3d1dc0a5c1993f
jmaki@openplans.org <jmaki@openplans.org@638cd66d-8850-0410-a654-37fb4b726c3a>
2011-02-11 23:54:55 +0000

    + CSS tweaks + Port number fix on sign
    git-svn-id: http://onebusaway.googlecode.com/svn/onebusaway-nyc/trunk@4049 638cd66d-8850-0410-a654-37fb4b726c3a

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/Sign.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/Sign.js
@@ -99,7 +99,7 @@ OBA.Sign = function() {
 							OBA.Config.infoBubbleFooterFunction("sign", stopId) +
 						'</th>' +
 						'<th class="qr">' + 
-							'<img src="http://' + window.location.hostname + ((window.location.port !== 80) ? ':' + window.location.port : '') + '/img/qr/' + stopId + '.png" alt="QR Code"/>' + 
+							'<img src="http://' + window.location.hostname + ((window.location.port !== '') ? ':' + window.location.port : '') + '/img/qr/' + stopId + '.png" alt="QR Code"/>' + 
 						'</th>' +
 					'</tr>' + 
 				 '</thead>')
