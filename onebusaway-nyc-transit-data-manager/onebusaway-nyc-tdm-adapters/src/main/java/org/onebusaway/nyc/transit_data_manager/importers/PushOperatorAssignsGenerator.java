7f5c54cae83e4fc1fbded9fb568a9cc675d31b6b
sheldonabrown <sheldonb@gmail.com>
2011-09-12 08:53:43 -0400

    update from latest

---
+++ onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-adapters/src/main/java/org/onebusaway/nyc/transit_data_manager/importers/PushOperatorAssignsGenerator.java
@@ -0,0 +1,74 @@
+package org.onebusaway.nyc.transit_data_manager.importers;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.onebusaway.nyc.transit_data_manager.model.MtaUtsCrewAssignment;
+import org.onebusaway.nyc.transit_data_manager.importers.tools.UtsMappingTool;
+import tcip_final_3_0_5_1.CPTPushHeader;
+import tcip_final_3_0_5_1.ObjectFactory;
+import tcip_final_3_0_5_1.SCHOperatorAssignment;
+import tcip_final_3_0_5_1.SchPushOperatorAssignments;
+import tcip_final_3_0_5_1.SchPushOperatorAssignments.Assignments;
+
+public class PushOperatorAssignsGenerator {
+
+	private GregorianCalendar headerCal;
+	private UtsMappingTool mappingTool = null;
+	
+	public PushOperatorAssignsGenerator(GregorianCalendar headerCal) {
+		super();
+		this.headerCal = headerCal;
+		this.mappingTool = new UtsMappingTool();
+	}
+	
+	public SchPushOperatorAssignments generateFromOpAssignList (List<SCHOperatorAssignment> opAssignList) {
+				
+		SchPushOperatorAssignments resultOpAssigns = new SchPushOperatorAssignments();
+		
+		resultOpAssigns.setPushHeader(generatePushHeader());
+		resultOpAssigns.setAssignments(generateAssignments(opAssignList));
+		
+		return resultOpAssigns;
+	}
+	
+	private CPTPushHeader generatePushHeader () {
+		DatatypeFactory df = null;
+		try {
+			df = DatatypeFactory.newInstance();
+		} catch (DatatypeConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+		XMLGregorianCalendar xmlHeaderCal = df.newXMLGregorianCalendar(headerCal);
+		
+		CPTPushHeader ph = new CPTPushHeader();
+		
+		ph.setFileType("operator-assignments-file");
+		ph.setEffective(mappingTool.dateToDateString(headerCal));
+		ph.setSource(0);
+		ph.setUpdatesOnly(false);
+		ph.setUpdatesThru(mappingTool.dateToDateString(headerCal));
+		ph.setTimeSent(mappingTool.timestampToDateString(headerCal));
+		
+		return ph;
+	}
+	
+	private SchPushOperatorAssignments.Assignments generateAssignments(List<SCHOperatorAssignment> assignmentList) {
+		SchPushOperatorAssignments.Assignments assignmentsBlock = new SchPushOperatorAssignments.Assignments();
+		
+		// iterate over assignmentList and add each element using assignmentsBlock.getAssignment().add()
+		Iterator<SCHOperatorAssignment> itr = assignmentList.iterator();
+		while (itr.hasNext()) {
+			assignmentsBlock.getAssignment().add(itr.next());
+		}
+		
+		return assignmentsBlock;
+	}
+}
