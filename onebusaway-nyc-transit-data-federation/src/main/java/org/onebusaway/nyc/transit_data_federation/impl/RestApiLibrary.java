5fbe8cc815fb614fd383fc7310977c26afe2bbe3
jeffmaki <jmaki@openplans.org>
2011-09-29 16:19:23 -0400

    Changes to pull from all SI depots + JG; change to allow port specification for TDM. Still need to move URL designation of TDM to data-sources.xml

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/RestApiLibrary.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/RestApiLibrary.java
@@ -1,7 +1,6 @@
 package org.onebusaway.nyc.transit_data_federation.impl;
 
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 
 public class RestApiLibrary {
@@ -10,9 +9,13 @@ public class RestApiLibrary {
 
 	private String _apiPrefix = "/api/";
 
-	public RestApiLibrary(String host, String apiPrefix) {
+	private int _port = 80;
+	
+	public RestApiLibrary(String host, Integer port, String apiPrefix) {
 		_host = host;
 		_apiPrefix = apiPrefix;
+		if(port != null)
+		  _port = port;
 	}
 	
 	public URL buildUrl(String baseObject, String... params) throws Exception {
@@ -22,18 +25,15 @@ public class RestApiLibrary {
 
 		if(params.length > 0) {
 			url += "/";
-		
 			for(int i = 0; i < params.length; i++) {
 				String param = params[i];
-
 				url += param;				
-				
 				if(i < params.length - 1)
 					url += "/";
 			}
 		}
-		
-		return new URI("http", _host, url, null).toURL();		
+
+		return new URL("http", _host, _port, url);
 	}	
 	
 	public void executeApiMethodWithNoResult(String baseObject, String... params)
@@ -41,7 +41,7 @@ public class RestApiLibrary {
 
 		URL requestUrl = buildUrl(baseObject, params);
 		HttpURLConnection conn = (HttpURLConnection)requestUrl.openConnection();
-
+		
 		if(conn.getResponseCode() != HttpURLConnection.HTTP_OK)
 			throw new Exception("TDM API method call " + requestUrl.toString() + " failed.");
 	}
