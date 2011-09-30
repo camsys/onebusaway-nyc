93c2b7b69a7a9c3a71aa01aff1c2bd3bdb2a2d8e
Denis Haskin <denis@ceromancy.net>
2011-09-29 21:39:46 -0400

    Persistence of bus location messages to db.

--- onebusaway-nyc-report-archive/src/test/java/org/onebusaway/nyc/report_archive/queue/ArchivingInputQueueListenerTaskTest.java
+++ onebusaway-nyc-report-archive/src/test/java/org/onebusaway/nyc/report_archive/queue/ArchivingInputQueueListenerTaskTest.java
@@ -2,19 +2,33 @@ package org.onebusaway.nyc.report_archive.queue;
 
 import static org.junit.Assert.*;
 
+import org.onebusaway.nyc.report_archive.services.CcLocationReportDao;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ArchivingInputQueueListenerTaskTest extends
-    ArchivingInputQueueListenerTask {
+@RunWith(MockitoJUnitRunner.class)
+public class ArchivingInputQueueListenerTaskTest {
 
+  @Mock
+  CcLocationReportDao dao;
+  
+  @InjectMocks
+  ArchivingInputQueueListenerTask t = new ArchivingInputQueueListenerTask();
+  
   @Test
   public void testProcessMessage() throws IOException {
-    String contents = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("sample-message.json"))).readLine(); 
-    processMessage("this is the address", contents);
+    String contents = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("sample-message.json"))).readLine();
+    t.processMessage("this is the address", contents);
   }
 
 }
