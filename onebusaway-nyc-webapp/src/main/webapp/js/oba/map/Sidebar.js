d586764510b65887d91a837dd2f26dc26ac357e2
Eva Shon <eshon@openplans.org>
2011-11-26 01:47:27 -0500

    Colored stop icons for left panel and loading message

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/map/Sidebar.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/map/Sidebar.js
@@ -24,6 +24,7 @@ OBA.Sidebar = function() {
 	var legend = jQuery("#legend");
 	var results = jQuery("#results");
 	var noResults = jQuery("#no-results");
+	var loading = jQuery("#loading");
 
 	function addSearchBehavior() {
 		var searchForm = jQuery("#searchbar form");
@@ -156,8 +157,8 @@ OBA.Sidebar = function() {
 				jQuery.each(destination.stops, function(__, stop) {
 					var stopLink = jQuery("<a href='#'></a>")
 									.text(stop.name);
-					
-					var stopItem = jQuery("<li></li>")
+
+					var stopItem = jQuery('<li class="r_' + routeResult.color + '"></li>')
 									.append(stopLink);
 	
 					stopsList.append(stopItem);
@@ -174,7 +175,6 @@ OBA.Sidebar = function() {
 					stopLink.mouseout(function(e) {
 						e.preventDefault();
 						routeMap.hideStopIcon(stop.stopId);
-						stopLink.children('.zoomToStop').remove();
 					});
 				});
 
@@ -243,6 +243,7 @@ OBA.Sidebar = function() {
 		welcome.hide();
 		legend.hide();
 		results.hide();
+		loading.show();
 
 		var resultsList = jQuery("#results ul");
 		var legendList = jQuery("#legend ul");
@@ -257,12 +258,12 @@ OBA.Sidebar = function() {
 			if(resultCount === 0) {
 				legend.hide();
 				results.hide();
-
 				noResults.show();
 				return;
 			} else {
 				noResults.hide();
 			}
+			loading.hide();
 
 			OBA.Config.analyticsFunction("Search", q + " [" + resultCount + "]");
 			
