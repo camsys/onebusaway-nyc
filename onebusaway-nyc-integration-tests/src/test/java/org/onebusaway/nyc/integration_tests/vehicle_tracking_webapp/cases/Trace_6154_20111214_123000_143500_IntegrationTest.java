161845e60ce5d004dc0783533c88e46d6a275660
brandon willard <brandonwillard@gmail.com>
2011-12-17 04:36:46 -0600

    new trace

---
+++ onebusaway-nyc-integration-tests/src/test/java/org/onebusaway/nyc/integration_tests/vehicle_tracking_webapp/cases/Trace_6154_20111214_123000_143500_IntegrationTest.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2011 Metropolitan Transportation Authority
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.onebusaway.nyc.integration_tests.vehicle_tracking_webapp.cases;
+
+public class Trace_6154_20111214_123000_143500_IntegrationTest extends AbstractTraceRunner {
+
+  public Trace_6154_20111214_123000_143500_IntegrationTest() throws Exception {
+    super("6154_20111214_123000_143500.csv.gz");
+    setBundle("si", "2011-12-07T00:00:00EDT");
+  }
+}
