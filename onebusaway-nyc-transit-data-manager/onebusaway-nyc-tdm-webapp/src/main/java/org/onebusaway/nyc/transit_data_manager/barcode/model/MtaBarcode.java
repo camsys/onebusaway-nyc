4c33129e2186f08b07c763b7b45b5eb1af3d8c93
Scott Clark <sclark@camsys.com>
2012-03-29 16:52:34 -0400

    Add hastus output, resolving OBANYC-752. also pull out some of the zip handling into a separate calss and better handle duplicate input stop ids.

--- onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-webapp/src/main/java/org/onebusaway/nyc/transit_data_manager/barcode/model/MtaBarcode.java
+++ onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-webapp/src/main/java/org/onebusaway/nyc/transit_data_manager/barcode/model/MtaBarcode.java
@@ -2,6 +2,10 @@ package org.onebusaway.nyc.transit_data_manager.barcode.model;
 
 public class MtaBarcode {
 
+  public MtaBarcode (String contents) {
+    this.contents = contents;
+  }
+  
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
@@ -12,38 +16,34 @@ public class MtaBarcode {
       return false;
     MtaBarcode other = (MtaBarcode) obj;
 
-    if ((other.getStopId() != -1 && getStopId() != -1) && (other.getStopId() == getStopId())) {
+    if ((other.getContents() != null && getContents() != null) && (other.getContents().equals(getContents()))) {
       return true;
-    } else if ((other.getStopIdStr() != null && getStopIdStr() != null) && (other.getStopIdStr().equalsIgnoreCase(getStopIdStr()))) {
-      return true;
-    } 
-
-    return false;
+    } else {
+      return false;
+    }
+    
   }
   
-  private int stopId = -1;
-  private String stopIdStr;
-  private String contents;
-  
-  public int getStopId() {
-    return stopId;
+  @Override
+  public int hashCode () {
+    return contents.hashCode();
   }
+  
+  private String contents = null;
+  private String stopIdStr = null;
+  
+  
   public String getStopIdStr() {
     return stopIdStr;
   }
-  public String getContents() {
-    return contents;
-  }
-  public void setStopId(int stopId) {
-    this.stopId = stopId;
-  }
+
   public void setStopIdStr(String stopIdStr) {
     this.stopIdStr = stopIdStr;
   }
-  public void setContents(String contents) {
-    this.contents = contents;
+
+  public String getContents() {
+    return contents;
   }
-  
   
   
 }
--- onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-webapp/src/test/java/org/onebusaway/nyc/transit_data_manager/api/barcode/BatchQrGeneratorResourceTest.java
+++ onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-webapp/src/test/java/org/onebusaway/nyc/transit_data_manager/api/barcode/BatchQrGeneratorResourceTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 import org.onebusaway.nyc.transit_data_manager.barcode.BarcodeImageType;
@@ -15,16 +15,14 @@ public class BatchQrGeneratorResourceTest extends QrCodeGeneratorResource {
 
   @Test
   public void testGenerateBarcodeZipFileFromUrlList() throws IOException {
-    List<MtaBarcode> bcList = new ArrayList<MtaBarcode>();
+    Set<MtaBarcode> bcList = new HashSet<MtaBarcode>();
     
-    MtaBarcode bcOne = new MtaBarcode();
+    MtaBarcode bcOne = new MtaBarcode("HTTP://BT.MTA.INFO/S/10");
     bcOne.setStopIdStr("10");
-    bcOne.setContents("HTTP://BT.MTA.INFO/S/10");
     bcList.add(bcOne);
     
-    MtaBarcode bcTwo = new MtaBarcode();
+    MtaBarcode bcTwo = new MtaBarcode("HTTP://BT.MTA.INFO/S/20");
     bcTwo.setStopIdStr("20");
-    bcTwo.setContents("HTTP://BT.MTA.INFO/S/20");
     bcList.add(bcTwo);
     
     File zipFile = generateBarcodeZipFileFromUrlList(bcList, 99, BarcodeImageType.PNG, 4);
@@ -33,7 +31,9 @@ public class BatchQrGeneratorResourceTest extends QrCodeGeneratorResource {
     
     int numActualEntries = tester.getNumEntriesInZipFile(zipFile);
     
-    assertEquals(bcList.size(), numActualEntries);
+    int numExpectedEntries = bcList.size() + 1; // Add one to account for hastus file.
+    
+    assertEquals(numExpectedEntries, numActualEntries);
   }
 
 }
