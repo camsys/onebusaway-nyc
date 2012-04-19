7164d8701c5befde3f6f493630c57eba86fc50d7
novalis <novalis@openplans.org>
2012-04-19 15:57:45 -0400

    add license, some doco to MultiCSVLogger

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/bundle/tasks/stif/MultiCSVLogger.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/bundle/tasks/stif/MultiCSVLogger.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2012 Metropolitan Transportation Authority
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.onebusaway.nyc.transit_data_federation.bundle.tasks.stif;
 
 import java.io.File;
@@ -10,6 +25,14 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Provides logging to multiple CSV files.  First call header("a,list,of,fields"),
+ * then log(any,object,for,fields);
+ * 
+ * TODO: tests, add checking to ensure that headers have same number of fields
+ * as entries
+ * 
+ */
 public class MultiCSVLogger {
   private Logger _log = LoggerFactory.getLogger(MultiCSVLogger.class);
 
