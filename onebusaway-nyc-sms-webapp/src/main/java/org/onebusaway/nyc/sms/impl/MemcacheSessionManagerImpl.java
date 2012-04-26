6432765b557971342022d1cd94f0f97d4ac5e907
Denis Haskin <denis@ceromancy.net>
2012-04-26 16:05:15 -0400

    Config for sms to use memcache for sessions.

--- onebusaway-nyc-sms-webapp/src/main/java/org/onebusaway/nyc/sms/impl/MemcacheSessionManagerImpl.java
+++ onebusaway-nyc-sms-webapp/src/main/java/org/onebusaway/nyc/sms/impl/MemcacheSessionManagerImpl.java
@@ -1,19 +1,20 @@
 package org.onebusaway.nyc.sms.impl;
 
-import java.io.IOException;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.concurrent.Future;
 
 import net.spy.memcached.CASValue;
 import net.spy.memcached.MemcachedClientIF;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemcacheSessionManagerImpl extends SessionManagerImpl {
 
   static Logger _log = LoggerFactory.getLogger(MemcacheSessionManagerImpl.class);
-  private int _expiryTime;
   
   @Autowired
   private MemcacheClientFactory memcacheClientFactory;
@@ -25,25 +26,32 @@ public class MemcacheSessionManagerImpl extends SessionManagerImpl {
   @Override
   public Map<String, Object> getContext(String key) {
     Map<String, Object> context = getContextFromCache(key);
-    if (context != null)
+    if (context != null) {
+      _log.info("getContext for " + key + " not null, updating local cache");
       updateLocalCache(key, context);
-    else
+    } else {
+      _log.info("getContext for " + key + " is null, delegating to superclass");
       context = super.getContext(key);
+    }
     return context;
   }
 
   @Override
   public boolean contextExistsFor(String sessionId) {
-    return contextExistsInCacheFor(sessionId) || super.contextExistsFor(sessionId);
+    boolean exists = contextExistsInCacheFor(sessionId) || super.contextExistsFor(sessionId);
+    _log.info("contextExistsFor returning " + exists + " for sessionId " + sessionId);
+    return exists;
   }
   
   @Override
   public void saveContext(String sessionId) {
+    _log.info("Saving context for sessionId " + sessionId);
     MemcachedClientIF client;
     try {
       client = memcacheClientFactory.getCacheClient();
-      client.cas(sessionId, _sessionTimeout, super.getContext(sessionId));
-    } catch (IOException e) {
+      Future<Boolean> cas = client.set(sessionId, _sessionTimeout, super.getContext(sessionId));
+      _log.info("saveContext: response: " + cas.get());
+    } catch (Exception e) {
       _log.error("Failed to save context: ", e);
     }
   }
@@ -53,11 +61,14 @@ public class MemcacheSessionManagerImpl extends SessionManagerImpl {
   @SuppressWarnings("unchecked")
   private Map<String, Object> getContextFromCache(String key) {
     MemcachedClientIF client;
+    _log.info("getContextFromCache for " + key);
     try {
       client = memcacheClientFactory.getCacheClient();
       CASValue<Object> value = client.getAndTouch(key, _sessionTimeout);
-      return (Map<String, Object>) (value == null ? value : value.getValue());
-    } catch (IOException e) {
+      Map<String, Object> map = (Map<String, Object>) (value == null ? value : value.getValue());
+      _log.info("getContextFromCache for " + key + " result is " + map);
+      return map;
+    } catch (Exception e) {
       _log.error("Failed to get context from cache: ", e);
     }
     return null;
@@ -67,14 +78,17 @@ public class MemcacheSessionManagerImpl extends SessionManagerImpl {
     MemcachedClientIF client;
     try {
       client = memcacheClientFactory.getCacheClient();
-      return client.get(sessionId) != null;
-    } catch (IOException e) {
-      _log.error("Failed to get context from cache: ", e);
+      boolean exists = client.get(sessionId) != null;
+      _log.info("contextExistsInCacheFor " + sessionId + " result is " + exists);
+      return exists;
+    } catch (Exception e) {
+      _log.error("Failed to check contextExistsInCacheFor: ", e);
     }
     return false;
   }
 
   private void updateLocalCache(String key, Map<String, Object> context) {
+    _log.info("updateLocalCache for key " + key);
     super.updateContext(key, context);
   }
 
