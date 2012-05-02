13d4b79158bb1810fc9ff685b7340c50c999967e
Adwait Belsare <abelsare@camsys.com>
2012-05-02 11:27:32 -0400

    Create admin war for OBANYC-936

---
+++ onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/webapp/actions/IndexAction.java
@@ -0,0 +1,12 @@
+package org.onebusaway.nyc.webapp.actions;
+
+import org.onebusaway.presentation.impl.NextActionSupport;
+
+public class IndexAction extends NextActionSupport {
+
+	/**
+	 * Generated
+	 */
+	private static final long serialVersionUID = 3264168200719504906L;
+
+}
