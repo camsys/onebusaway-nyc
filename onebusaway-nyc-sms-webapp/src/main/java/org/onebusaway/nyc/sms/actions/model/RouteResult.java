348a349a351041b2c70659d639c27b7ae077e772
Aaron Sutula <asutula@openplans.org>
2012-05-08 17:39:09 -0400

    Refactoring sms model for new SearchResult interface requirements.

--- onebusaway-nyc-sms-webapp/src/main/java/org/onebusaway/nyc/sms/actions/model/RouteResult.java
+++ onebusaway-nyc-sms-webapp/src/main/java/org/onebusaway/nyc/sms/actions/model/RouteResult.java
@@ -8,28 +8,41 @@ import java.util.List;
 
 /**
  * Route as a top-level search result.
+ * 
  * @author jmaki
- *
+ * 
  */
 public class RouteResult implements SearchResult, Serializable {
-  
-  private static final long serialVersionUID = 1L;
 
-  private RouteBean route;
-  
-  private List<RouteDirection> directions;
-      
-  public RouteResult(RouteBean route, List<RouteDirection> directions) {
-    this.route = route;
-    this.directions = directions;
-  }
-  
-  public String getShortName() {
-    return route.getShortName();
-  }
-  
-  public List<RouteDirection> getDirections() {
-    return directions;
-  }  
-  
+	private static final long serialVersionUID = 1L;
+
+	private RouteBean route;
+
+	private List<RouteDirection> directions;
+
+	public RouteResult(RouteBean route, List<RouteDirection> directions) {
+		this.route = route;
+		this.directions = directions;
+	}
+
+	public String getShortName() {
+		return route.getShortName();
+	}
+
+	public List<RouteDirection> getDirections() {
+		return directions;
+	}
+
+	@Override
+	public void setDistanceToQueryLocation(Double distance) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Double getDistanceToQueryLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
