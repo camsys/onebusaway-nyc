75a6dbd8d4846dac9ac6e813efcaf2ee90e62332
sheldonabrown <sheldonb@gmail.com>
2012-05-31 13:54:31 -0400

    re-implemented email implementation based on aws example from SDK, in hopes of debugging 403

--- onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/impl/EmailServiceImpl.java
+++ onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/impl/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import org.onebusaway.nyc.admin.service.EmailService;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AWSJavaMailTransport;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient;
 import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
@@ -18,9 +19,14 @@ import org.springframework.web.context.ServletContextAware;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.Future;
 
 import javax.annotation.PostConstruct;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletContext;
 
 public class EmailServiceImpl implements EmailService, ServletContextAware {
@@ -30,6 +36,9 @@ public class EmailServiceImpl implements EmailService, ServletContextAware {
   AmazonSimpleEmailServiceAsyncClient _eClient;
   private String _username;
   private String _password;
+  private Properties _properties;
+  private Session _session;
+  private Transport _transport;
   
   @Override
   public void setSmtpUser(String user) {
@@ -44,8 +53,18 @@ public class EmailServiceImpl implements EmailService, ServletContextAware {
   @Override
   public void setup() {
     try {
+      // AWS specifics
       _credentials = new BasicAWSCredentials(_username, _password);
       _eClient = new AmazonSimpleEmailServiceAsyncClient(_credentials);
+      // Java specifics
+    _properties = new Properties();
+    _properties.setProperty("mail.transport.protocol", "aws");
+    _properties.setProperty("mail.aws.user", _credentials.getAWSAccessKeyId());
+    _properties.setProperty("mail.aws.password", _credentials.getAWSSecretKey());
+    _session = Session.getInstance(_properties);
+    _transport = new AWSJavaMailTransport(_session, null);
+    
+
     } catch (Exception ioe) {
       _log.error(ioe.toString());
       throw new RuntimeException(ioe);
@@ -69,6 +88,10 @@ public class EmailServiceImpl implements EmailService, ServletContextAware {
 
   @Override
   public void send(String to, String from, String subject, StringBuffer messageBody) {
+    sendJava(to, from, subject, messageBody);
+  }
+  
+  public void sendSES(String to, String from, String subject, StringBuffer messageBody) {
     List<String> toAddresses = new ArrayList<String>();
     for (String address : to.split(",")) {
       toAddresses.add(address);
@@ -82,6 +105,24 @@ public class EmailServiceImpl implements EmailService, ServletContextAware {
     _log.info("sent email to " + to + " with result=" + result);
   }
 
+  public void sendJava(String to, String from, String subject, StringBuffer messageBody) {
+    try {
+      javax.mail.Message msg = new MimeMessage(_session);
+      msg.setFrom(new InternetAddress(from));
+      msg.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+      msg.setSubject(subject);
+      if (messageBody != null) { 
+        msg.setText(messageBody.toString());
+      } 
+      msg.saveChanges();
+      if (!_transport.isConnected()) {
+        _transport.connect();
+      }
+      _transport.sendMessage(msg, null);
+    } catch (Exception e) {
+      _log.error("sendJava failed", e);
+    }
+  }
   @Override
   public void setServletContext(ServletContext servletContext) {
     if (servletContext != null) {
