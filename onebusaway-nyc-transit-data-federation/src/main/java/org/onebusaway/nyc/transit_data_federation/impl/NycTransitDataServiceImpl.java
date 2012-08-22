5e1113119b82607e12cd8a000eb762541097490a
Aaron Sutula <asutula@openplans.org>
2012-08-22 13:00:09 -0400

    Initial try at removing hessian from front end web app

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/NycTransitDataServiceImpl.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/NycTransitDataServiceImpl.java
@@ -88,7 +88,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-class NycTransitDataServiceImpl implements TransitDataService, NycTransitDataService {
+class NycTransitDataServiceImpl implements NycTransitDataService {
 	
   private static Logger _log = LoggerFactory.getLogger(NycTransitDataServiceImpl.class);
 
