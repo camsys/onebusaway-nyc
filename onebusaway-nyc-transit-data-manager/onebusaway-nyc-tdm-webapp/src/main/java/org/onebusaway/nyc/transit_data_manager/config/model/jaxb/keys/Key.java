ff06c199f359fa652db462e064cd1cee6330e651
Denis Haskin <denis@ceromancy.net>
2012-09-28 16:01:09 -0400

    OBANYC-1099 - create API key creation and management webservices.

---
+++ onebusaway-nyc-transit-data-manager/onebusaway-nyc-tdm-webapp/src/main/java/org/onebusaway/nyc/transit_data_manager/config/model/jaxb/keys/Key.java
@@ -0,0 +1,14 @@
+package org.onebusaway.nyc.transit_data_manager.config.model.jaxb.keys;
+
+public class Key {
+  private String keyvalue;
+
+  public String getKeyvalue() {
+    return keyvalue;
+  }
+
+  public void setKeyvalue(String keyvalue) {
+    this.keyvalue = keyvalue;
+  }
+  
+}
