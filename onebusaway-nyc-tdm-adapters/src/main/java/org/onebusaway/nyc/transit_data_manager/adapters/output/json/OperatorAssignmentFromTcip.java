d05e3bc3938828d33d2c7b518221b5c6baf84f62
Adwait Belsare <abelsare@camsys.com>
2012-11-15 15:02:38 -0500

    Fix for OBANYC-1872. Remove leading zeros from pass number

--- onebusaway-nyc-tdm-adapters/src/main/java/org/onebusaway/nyc/transit_data_manager/adapters/output/json/OperatorAssignmentFromTcip.java
+++ onebusaway-nyc-tdm-adapters/src/main/java/org/onebusaway/nyc/transit_data_manager/adapters/output/json/OperatorAssignmentFromTcip.java
@@ -4,10 +4,8 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.onebusaway.nyc.transit_data_manager.adapters.ModelCounterpartConverter;
-import org.onebusaway.nyc.transit_data_manager.adapters.api.processes.UTSUtil;
 import org.onebusaway.nyc.transit_data_manager.adapters.output.model.json.OperatorAssignment;
 import org.onebusaway.nyc.transit_data_manager.adapters.tools.TcipMappingTool;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import tcip_final_3_0_5_1.SCHOperatorAssignment;
 
@@ -23,11 +21,10 @@ public class OperatorAssignmentFromTcip implements ModelCounterpartConverter<SCH
 
 	public OperatorAssignment convert(SCHOperatorAssignment input) {
 		OperatorAssignment opAssign = new OperatorAssignment();
-		UTSUtil util = new UTSUtil();
 		TcipMappingTool mappingTool = new TcipMappingTool();
 
 		opAssign.setAgencyId(mappingTool.getJsonModelAgencyIdByTcipId(input.getOperator().getAgencyId()));
-		opAssign.setPassId(util.stripLeadingCharacters(mappingTool.cutPassIdFromOperatorDesignator(input.getOperator().getDesignator())));
+		opAssign.setPassId(new Long(input.getOperator().getOperatorId()).toString());
 
 		opAssign.setRunRoute(input.getLocalSCHOperatorAssignment().getRunRoute());
 		opAssign.setRunNumber(mappingTool.cutRunNumberFromTcipRunDesignator(input.getRun().getDesignator()));
