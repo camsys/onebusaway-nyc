4900eff73b9d35de71b15ad3d78f249bcb010b47
novalis <novalis@openplans.org>
2012-11-20 10:42:18 -0500

    use new holiday code field

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/bundle/tasks/stif/model/ServiceCode.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/bundle/tasks/stif/model/ServiceCode.java
@@ -101,4 +101,9 @@ public enum ServiceCode {
 	  return letterCodeForServiceCode.get(this);
 	}
 
+	public boolean isHoliday() {
+    return !(this == WEEKDAY_SCHOOL_OPEN || this == WEEKDAY_SCHOOL_CLOSED
+        || this == SATURDAY || this == SUNDAY);
+	}
+
 }
