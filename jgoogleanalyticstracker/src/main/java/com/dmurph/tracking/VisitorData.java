b04e5ca8c67c17fad3444762a766f6b69e5bea85
denishaskin <denis@constantorbit.com>
2012-11-28 12:44:31 -0500

    VisitorData from jgoogleanalyticstracker needs to be serializable for session storage in memcache.

--- jgoogleanalyticstracker/src/main/java/com/dmurph/tracking/VisitorData.java
+++ jgoogleanalyticstracker/src/main/java/com/dmurph/tracking/VisitorData.java
@@ -1,8 +1,11 @@
 package com.dmurph.tracking;
 
+import java.io.Serializable;
 import java.security.SecureRandom;
 
-public class VisitorData {
+public class VisitorData implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private int visitorId;
 	private long timestampFirst;
 	private long timestampPrevious;
