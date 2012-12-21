9decff53f27bc86773b75d24102207e5fb1f40a0
jeffmaki <jmaki@openplans.org>
2012-12-21 14:00:35 -0500

    Cleanup before merge

--- onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/BlockStateService.java
+++ onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/BlockStateService.java
@@ -28,7 +28,6 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 
 import org.onebusaway.collections.Min;
-import org.onebusaway.container.cache.Cacheable;
 import org.onebusaway.container.refresh.Refreshable;
 import org.onebusaway.geospatial.model.CoordinateBounds;
 import org.onebusaway.geospatial.model.CoordinatePoint;
@@ -397,20 +396,17 @@ public class BlockStateService {
 
     final Map<BlockLocationKey, BestBlockStates> results = Maps.newHashMap();
 
-    final NycRawLocationRecord record = observation.getRecord();
-    
     GregorianCalendar gc = new GregorianCalendar();
     gc.setTimeInMillis(observation.getTime());
     gc.set(GregorianCalendar.MINUTE, (int)Math.floor(gc.get(GregorianCalendar.MINUTE) / 15) * 60);
     gc.set(GregorianCalendar.SECOND, 0);
-    final long time = gc.getTimeInMillis();    
-    
+    final long time = gc.getTimeInMillis();        
     final Date timeFrom = new Date(time - _tripSearchTimeAfterLastStop);
     final Date timeTo = new Date(time + _tripSearchTimeBeforeFirstStop);
 
+    final NycRawLocationRecord record = observation.getRecord();
     final CoordinateBounds bounds = SphericalGeometryLibrary.bounds(
         record.getLatitude(), record.getLongitude(), _tripSearchRadius);
-
     final Coordinate obsPoint = new Coordinate(record.getLongitude(),
         record.getLatitude());
     final Envelope searchEnv = new Envelope(bounds.getMinLon(),
@@ -449,7 +445,7 @@ public class BlockStateService {
           if (!blockToActiveInstances.containsKey(block.getId())) {
             instances = blockToActiveInstances.get(block.getId());
             instances.addAll(_blockCalendarService.getActiveBlocks(
-                    block.getId(), timeFrom.getTime(), timeTo.getTime()));
+                block.getId(), timeFrom.getTime(), timeTo.getTime()));
             if(!instances.isEmpty())
             	blockToActiveInstances.putAll(block.getId(), instances);
           } else {
