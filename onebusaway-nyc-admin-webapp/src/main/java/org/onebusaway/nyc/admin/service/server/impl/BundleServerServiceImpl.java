33c854ea1a7d6dfe95a603550d8939d8809074d9
sheldonabrown <sheldonb@gmail.com>
2013-01-17 16:50:56 -0500

    tweak to make bundle server more resilient to AWS timeouts

--- onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/server/impl/BundleServerServiceImpl.java
+++ onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/server/impl/BundleServerServiceImpl.java
@@ -102,16 +102,22 @@ public class BundleServerServiceImpl implements BundleServerService, ServletCont
   }
 
   private Instance getInstance(String instanceId) {
-    DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest();
-    List<String> list = new ArrayList<String>();
-    list.add(instanceId);
-    describeInstancesRequest.setInstanceIds(list);
-    DescribeInstancesResult result = _ec2.describeInstances(describeInstancesRequest);
-    if (!result.getReservations().isEmpty()) {
-      if (!result.getReservations().get(0).getInstances().isEmpty()) {
-        Instance i = result.getReservations().get(0).getInstances().get(0);
-        return i;
+    try {
+      DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest();
+      List<String> list = new ArrayList<String>();
+      list.add(instanceId);
+      describeInstancesRequest.setInstanceIds(list);
+      // this call can timeout and throw AmazonClientException if AWS is having
+      // a bad day
+      DescribeInstancesResult result = _ec2.describeInstances(describeInstancesRequest);
+      if (!result.getReservations().isEmpty()) {
+        if (!result.getReservations().get(0).getInstances().isEmpty()) {
+          Instance i = result.getReservations().get(0).getInstances().get(0);
+          return i;
+        }
       }
+    } catch (Exception e) {
+      _log.error("Call to AWS threw exception", e);
     }
     return null;
   }
