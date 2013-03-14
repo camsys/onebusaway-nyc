ae6d47ce5e754da783e679adcb2b2101c885f25f
sheldonabrown <sheldonb@gmail.com>
2013-03-14 12:37:41 -0400

    obanyc-1973 updating ModifyStartDateTask to work with MTABC GTFS.

--- onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/bundle/task/ModifyStartDateTask.java
+++ onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/service/bundle/task/ModifyStartDateTask.java
@@ -12,19 +12,20 @@ import java.util.Calendar;
 import java.util.Collection;
 
 /**
- * To help with major pick changes (bundle swaps that are completely different
- * bundles, not just updates), modify the start date of the gtfs to be start-1
+ * To help with pick changes (as opposed to just updates), modify the start date of the gtfs 
  * to allow inference of overnight routes.
  * 
- *  This task must be inserted before the calendar_service task to be effective.
- *
+ * This task must be inserted before the calendar_service task to be effective.
+ * 
  */
 public class ModifyStartDateTask implements Runnable {
-  
+
+  private static final int ROLLBACK_INTERVAL = -15;
+
   private Logger _log = LoggerFactory.getLogger(ModifyStartDateTask.class);
-  
+
   private GtfsMutableRelationalDao _gtfsMutableRelationalDao;
-  
+
   @Autowired
   public void setGtfsMutableRelationalDao(
       GtfsMutableRelationalDao gtfsMutableRelationalDao) {
@@ -35,12 +36,36 @@ public class ModifyStartDateTask implements Runnable {
     Collection<ServiceCalendar> calendars = _gtfsMutableRelationalDao.getAllCalendars();
     _log.info("found " + calendars.size() + " calendar entries");
     for (ServiceCalendar sc : calendars) {
-      Calendar cal = Calendar.getInstance();
-      cal.setTime(sc.getStartDate().getAsDate());
-      cal.add(Calendar.DAY_OF_YEAR, -1);  //go back a day
-      _log.info("changed calendar start date from " + sc.getStartDate().getAsDate()
-          + " to " + cal.getTime());
-      sc.setStartDate(new ServiceDate(cal));
+      if (isApplicable(sc)) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(sc.getStartDate().getAsDate());
+        cal.add(Calendar.DAY_OF_YEAR, ROLLBACK_INTERVAL); // go back 15 days recomputing month if necessary
+        _log.info("changed (" + sc.getServiceId() + ") calendar start date from "
+            + sc.getStartDate().getAsDate() + " to " + cal.getTime());
+        sc.setStartDate(new ServiceDate(cal));
+      }
     }
+  }
+
+  /**
+   * Pick changes happen on Saturday night; extend Saturday only serviceIds back more than two weeks 
+   * so that service will be considered.  Do not extends service exceptions that consider only a single 
+   * Saturday
+   * @param ServiceCalencar an entry in calendars.txt
+   * @return true if the calendar entry is applicable
+   */
+  private boolean isApplicable(ServiceCalendar sc) {
+    if (sc.getSaturday() == 1 
+        && sc.getMonday() == 0 
+        && sc.getTuesday() == 0
+        && sc.getWednesday() == 0 
+        && sc.getThursday() == 0
+        && sc.getFriday() == 0 
+        && sc.getSunday() == 0) {
+      if (!sc.getStartDate().equals(sc.getEndDate())) {
+        return true;
+      }
+    }
+    return false;
   }
 }
