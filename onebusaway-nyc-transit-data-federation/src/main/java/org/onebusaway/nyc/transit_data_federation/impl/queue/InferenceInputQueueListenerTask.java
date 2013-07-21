17df46b44493aef53d0608d0d9fbd37422d801b1
Allen Tang <natengall@gmail.com>
2013-07-20 23:23:20 -0400

    Fixed OBANYC-1844: UIs should discard old-queue data

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/queue/InferenceInputQueueListenerTask.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/queue/InferenceInputQueueListenerTask.java
@@ -15,9 +15,10 @@
  */
 package org.onebusaway.nyc.transit_data_federation.impl.queue;
 
+import org.onebusaway.container.refresh.Refreshable;
 import org.onebusaway.nyc.transit_data.model.NycQueuedInferredLocationBean;
 import org.onebusaway.nyc.transit_data_federation.services.predictions.PredictionIntegrationService;
-import org.onebusaway.nyc.util.configuration.ConfigurationService;
+import org.onebusaway.nyc.util.impl.tdm.ConfigurationServiceImpl;
 import org.onebusaway.realtime.api.EVehiclePhase;
 import org.onebusaway.realtime.api.VehicleLocationListener;
 import org.onebusaway.realtime.api.VehicleLocationRecord;
@@ -25,51 +26,102 @@ import org.onebusaway.transit_data_federation.services.AgencyAndIdLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * This component listens to the inference output queue and injects records into the local TDF/TDS.
+ * This component listens to the inference output queue and injects records into
+ * the local TDF/TDS.
  * 
  * @author jmaki
- *
+ * @author altang
+ * 
  */
+
 public class InferenceInputQueueListenerTask extends InferenceQueueListenerTask {
 
-  @Autowired
-  private VehicleLocationListener _vehicleLocationListener;
+	@Autowired
+	private VehicleLocationListener _vehicleLocationListener;
 
-  @Autowired
-  private PredictionIntegrationService _predictionIntegrationService;
+	@Autowired
+	private PredictionIntegrationService _predictionIntegrationService;
 
-  @Autowired
-  private ConfigurationService _configurationService;
+	private boolean useTimePredictions = false;
+	private boolean checkAge = false;
+	private int ageLimit = 300;
+	private boolean refreshCheck;
 
-  public String getQueueDisplayName() {
-    return "InferenceInputQueueListenerTask";
-  }
-  
-  private Boolean useTimePredictionsIfAvailable() {
-    return Boolean.parseBoolean(_configurationService.getConfigurationValueAsString("display.useTimePredictions", "false"));
-  }
+	public InferenceInputQueueListenerTask() {
+		setConfigurationService(new ConfigurationServiceImpl());
+		refreshCache();
+	}
 
-  @Override
-  protected void processResult(NycQueuedInferredLocationBean inferredResult, String contents) {
-    VehicleLocationRecord vlr = new VehicleLocationRecord();
-    vlr.setVehicleId(AgencyAndIdLibrary.convertFromString(inferredResult.getVehicleId()));
-    vlr.setTimeOfRecord(inferredResult.getRecordTimestamp());
-    vlr.setTimeOfLocationUpdate(inferredResult.getRecordTimestamp());
-    vlr.setBlockId(AgencyAndIdLibrary.convertFromString(inferredResult.getBlockId()));
-    vlr.setTripId(AgencyAndIdLibrary.convertFromString(inferredResult.getTripId()));
-    vlr.setServiceDate(inferredResult.getServiceDate());
-    vlr.setDistanceAlongBlock(inferredResult.getDistanceAlongBlock());
-    vlr.setCurrentLocationLat(inferredResult.getInferredLatitude());
-    vlr.setCurrentLocationLon(inferredResult.getInferredLongitude());
-    vlr.setPhase(EVehiclePhase.valueOf(inferredResult.getPhase()));
-    vlr.setStatus(inferredResult.getStatus());
+	public String getQueueDisplayName() {
+		return "InferenceInputQueueListenerTask";
+	}
 
-    _vehicleLocationListener.handleVehicleLocationRecord(vlr);
+	protected void setPredictionIntegrationService( PredictionIntegrationService predictionIntegrationService) {
+		_predictionIntegrationService = predictionIntegrationService;
+	}
 
-    // if we're updating time predictions with the generation service, tell the integration service to fetch
-    // a new set of predictions now that the TDS has been updated appropriately.
-    if(useTimePredictionsIfAvailable()) 
-      _predictionIntegrationService.updatePredictionsForVehicle(vlr.getVehicleId());
-  }
+	protected void setVehicleLocationListener( VehicleLocationListener vehicleLocationListener) {
+		_vehicleLocationListener = vehicleLocationListener;
+	}
 
+	protected void setConfigurationService(ConfigurationServiceImpl config) {
+		_configurationService = config;
+		refreshCache();
+	}
+	
+	@Refreshable(dependsOn = {"display.checkAge", "display.useTimePredictions", "display.ageLimit"})
+	protected void refreshCache() {
+		checkAge = Boolean.parseBoolean(_configurationService.getConfigurationValueAsString("display.checkAge", "false"));
+		useTimePredictions = Boolean.parseBoolean(_configurationService.getConfigurationValueAsString("display.useTimePredictions", "false"));
+		ageLimit = Integer.parseInt(_configurationService.getConfigurationValueAsString("display.ageLimit", "300"));
+	}
+	 
+	protected boolean getRefreshCheck() {
+		return refreshCheck;
+	}
+
+	protected boolean getUseTimePredictions() {
+		return useTimePredictions;
+	}
+	
+	protected long getAgeLimit() {
+		return ageLimit;
+	}
+
+	protected long computeTimeDifference(long timestamp) {
+		return (System.currentTimeMillis() - timestamp) / 1000; // output in seconds
+	}
+
+	@Override
+	protected void processResult(NycQueuedInferredLocationBean inferredResult, String contents) {
+		VehicleLocationRecord vlr = new VehicleLocationRecord();
+		if (checkAge) {
+			long difference = computeTimeDifference(inferredResult.getRecordTimestamp());
+			if (difference > ageLimit) {
+				_log.info("VehicleLocationRecord for "+ inferredResult.getVehicleId() + " discarded.");
+				return;
+			}
+		}
+		vlr.setVehicleId(AgencyAndIdLibrary.convertFromString(inferredResult.getVehicleId()));
+		vlr.setTimeOfRecord(inferredResult.getRecordTimestamp());
+		vlr.setTimeOfLocationUpdate(inferredResult.getRecordTimestamp());
+		vlr.setBlockId(AgencyAndIdLibrary.convertFromString(inferredResult.getBlockId()));
+		vlr.setTripId(AgencyAndIdLibrary.convertFromString(inferredResult.getTripId()));
+		vlr.setServiceDate(inferredResult.getServiceDate());
+		vlr.setDistanceAlongBlock(inferredResult.getDistanceAlongBlock());
+		vlr.setCurrentLocationLat(inferredResult.getInferredLatitude());
+		vlr.setCurrentLocationLon(inferredResult.getInferredLongitude());
+		vlr.setPhase(EVehiclePhase.valueOf(inferredResult.getPhase()));
+		vlr.setStatus(inferredResult.getStatus());
+		if (_vehicleLocationListener != null) {
+			_vehicleLocationListener.handleVehicleLocationRecord(vlr);
+		}
+		if (useTimePredictions) {
+			// if we're updating time predictions with the generation service,
+			// tell the integration service to fetch
+			// a new set of predictions now that the TDS has been updated
+			// appropriately.
+			_predictionIntegrationService.updatePredictionsForVehicle(vlr.getVehicleId());
+		}
+	}
 }
---
+++ onebusaway-nyc-transit-data-federation/src/test/java/org/onebusaway/nyc/transit_data_federation/impl/queue/InferenceInputQueueListenerTaskTest.java
@@ -0,0 +1,154 @@
+package org.onebusaway.nyc.transit_data_federation.impl.queue;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onebusaway.container.refresh.RefreshService;
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.nyc.transit_data.model.NycQueuedInferredLocationBean;
+import org.onebusaway.nyc.transit_data_federation.impl.predictions.QueuePredictionIntegrationServiceImpl;
+import org.onebusaway.nyc.transit_data_federation.impl.queue.InferenceInputQueueListenerTask;
+import org.onebusaway.nyc.transit_data_federation.services.predictions.PredictionIntegrationService;
+
+import org.onebusaway.nyc.util.impl.RestApiLibrary;
+import org.onebusaway.nyc.util.impl.tdm.ConfigurationServiceImpl;
+import org.onebusaway.nyc.util.impl.tdm.TransitDataManagerApiLibrary;
+import org.onebusaway.realtime.api.VehicleLocationListener;
+import org.onebusaway.realtime.api.VehicleLocationRecord;
+
+import eu.datex2.schema._1_0._1_0.Time;
+
+import java.net.URL;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InferenceInputQueueListenerTaskTest {
+
+	@Mock
+	private RefreshService refreshService;
+
+	@Mock
+	private TransitDataManagerApiLibrary mockApiLibrary;
+
+	@Mock
+	private RestApiLibrary mockRestApiLibrary;
+
+	@InjectMocks
+	private ConfigurationServiceImpl service;
+
+	private NycQueuedInferredLocationBean inferredResult;
+	private InferenceInputQueueListenerTask task;
+	private Long timestamp = new Long(System.currentTimeMillis());
+	private VehicleLocationListener listener;
+	private PredictionIntegrationService pService;
+
+	public NycQueuedInferredLocationBean initializeBean(){
+		NycQueuedInferredLocationBean bean = new NycQueuedInferredLocationBean();
+		bean.setVehicleId("MTA NYCT_123456789");
+		bean.setRecordTimestamp(timestamp);	
+		bean.setBlockId("sample_blockID");
+		bean.setTripId("sample_tripID");
+		bean.setServiceDate(Long.parseLong("1374120000000"));
+		bean.setDistanceAlongBlock(Double.parseDouble("9379.514420929047"));
+		bean.setInferredLatitude(Double.parseDouble("40.82360253803387"));
+		bean.setInferredLongitude(Double.parseDouble("-73.85864343194719"));
+		bean.setPhase("IN_PROGRESS");
+		bean.setStatus("sample_Status");
+		return bean;
+	}
+
+	@Before
+	public void setupApiLibrary() throws Exception {
+		task = new InferenceInputQueueListenerTask();
+		task.setConfigurationService(service);
+		listener = mock(VehicleLocationListener.class);
+		pService =  mock(QueuePredictionIntegrationServiceImpl.class);
+		task.setPredictionIntegrationService(pService);
+		task.setVehicleLocationListener(listener);
+	}
+
+	@Test
+	public void setValue() throws Exception {
+		RestApiLibrary ral = new RestApiLibrary("localhost", null, "api");
+		String json = new String("{\"config\":[{\"value\":\"20\",\"key\":\"tdm.crewAssignmentRefreshInterval\",\"description\":null,\"value-type\":\"String\",\"units\":\"minutes\",\"component\":\"tdm\",\"updated\":null}],\"status\":\"OK\"}");
+		when(mockApiLibrary.getItemsForRequest("config", "list")).thenReturn(ral.getJsonObjectsForString(json));
+		TransitDataManagerApiLibrary tdmal = new TransitDataManagerApiLibrary("tdm.staging.obanyc.com", 80, "/api");
+		URL setUrl = tdmal.buildUrl("config", "testComponent", "test123", "set");
+		when(mockRestApiLibrary.setContents(setUrl, "testValue")).thenReturn(true);
+		service.refreshConfiguration();
+		service.setConfigurationValue("testComponent", "test123", "testValue");
+		assertEquals(service.getConfigurationValueAsString("test123", null), "testValue");
+	}
+
+	@Test
+	public void testCheckAge() throws Exception {
+		inferredResult = initializeBean();
+		service.setConfigurationValue("display", "display.checkAge", "true");
+		task.refreshCache();
+
+		long defaultLimit = task.getAgeLimit();
+
+		//one second over default configuration for age limit
+		inferredResult.setRecordTimestamp(timestamp.longValue()-((defaultLimit+1)*1000));
+		task.processResult(inferredResult, "");
+		verify(listener,times(0)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+
+		//one second under default configuration for age limit
+		inferredResult.setRecordTimestamp(timestamp.longValue()-((defaultLimit-1)*1000));
+		task.processResult(inferredResult, "");
+		verify(listener,times(1)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+
+		//age limit lowered by a minute
+		service.setConfigurationValue("display", "display.ageLimit", Long.toString(defaultLimit-60));
+		task.refreshCache();
+		task.processResult(inferredResult, "");
+		verify(listener,times(1)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+
+		//age limit increased by a minute
+		service.setConfigurationValue("display", "display.ageLimit", Long.toString(defaultLimit+60));
+		task.refreshCache();
+		task.processResult(inferredResult, "");
+		verify(listener,times(2)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+	}
+
+	@Test
+	public void testComputeTimeDifference() throws Exception {
+		assertEquals(task.computeTimeDifference(timestamp), 0);
+		assertTrue(task.computeTimeDifference(timestamp-50000) < 300);
+		assertFalse(task.computeTimeDifference(timestamp-350000) < 300);
+	}
+
+	@Test
+	public void testDefaultConfig() throws Exception {
+		inferredResult = initializeBean();
+		task.processResult(inferredResult, "");
+		verify(listener, times(1)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+		verify(pService, times(0)).updatePredictionsForVehicle((AgencyAndId)any());
+	}
+
+	@Test
+	public void testModifiedConfig() throws Exception {
+		assertFalse(Boolean.parseBoolean(service.getConfigurationValueAsString("display.useTimePredictions", "error")));
+		assertFalse(Boolean.parseBoolean(service.getConfigurationValueAsString("display.checkAge", "error")));
+		service.setConfigurationValue("display", "display.useTimePredictions", "true");
+		service.setConfigurationValue("display", "display.checkAge", "true");
+		assertTrue(Boolean.parseBoolean(service.getConfigurationValueAsString("display.useTimePredictions", "error")));
+		assertTrue(Boolean.parseBoolean(service.getConfigurationValueAsString("display.checkAge", "error")));
+	}
+
+	@Test
+	public void testUseTimePredictions() throws Exception {
+		inferredResult = initializeBean();
+		service.setConfigurationValue("display", "display.useTimePredictions", "true");
+		task.refreshCache();
+		task.processResult(inferredResult, "");
+		verify(listener,times(1)).handleVehicleLocationRecord((VehicleLocationRecord)any());
+		verify(pService, times(1)).updatePredictionsForVehicle((AgencyAndId)any());
+	}
+}
+
