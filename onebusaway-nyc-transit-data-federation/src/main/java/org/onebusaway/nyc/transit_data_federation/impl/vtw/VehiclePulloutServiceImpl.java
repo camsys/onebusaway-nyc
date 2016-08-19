5e0109e5e35ec8c8654b4c6873b603467ae0aedb
lcaraballo <lennycaraballo@gmail.com>
2016-08-19 14:20:37 -0400

    Added configurable option to enable/disable VehiclePulloutService

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/vtw/VehiclePulloutServiceImpl.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/vtw/VehiclePulloutServiceImpl.java
@@ -41,6 +41,8 @@ public class VehiclePulloutServiceImpl implements VehiclePulloutService {
   private static Logger _log = LoggerFactory.getLogger(VehiclePulloutServiceImpl.class);
 
   private ScheduledFuture<VehiclePulloutServiceImpl.UpdateThread> _updateTask = null;
+  
+  private boolean _enabled;
 
   @Autowired
   private ThreadPoolTaskScheduler _taskScheduler;
@@ -76,6 +78,7 @@ public class VehiclePulloutServiceImpl implements VehiclePulloutService {
       String errorCode = schPulloutList.getErrorCode();
       if (errorCode != null && !errorCode.equals("0")){
         if (errorCode.equalsIgnoreCase("1")) {
+          _log.debug(response);
           _log.warn("Pullout list contained no pullouts.");
           return;
         }
@@ -110,8 +113,9 @@ public class VehiclePulloutServiceImpl implements VehiclePulloutService {
   }
   
   private void startUpdateProcess() {
+	  Integer updateIntervalSecs = _configurationService.getConfigurationValueAsInteger("tdm.vehiclePipoRefreshInterval", 60);
 	  if (_updateTask==null) {
-		  setUpdateFrequency(60); //1min
+		  setUpdateFrequency(updateIntervalSecs);
 	  }
   }
   
@@ -120,7 +124,9 @@ public class VehiclePulloutServiceImpl implements VehiclePulloutService {
     if (_updateTask != null) {
       _updateTask.cancel(true);
     }
-    _updateTask = _taskScheduler.scheduleWithFixedDelay(new UpdateThread(), seconds * 1000);
+    if(_enabled){
+    	_updateTask = _taskScheduler.scheduleWithFixedDelay(new UpdateThread(), seconds * 1000);
+    }
   }
 
   private class UpdateThread extends TimerTask {
@@ -135,12 +141,13 @@ public class VehiclePulloutServiceImpl implements VehiclePulloutService {
     }
   }
 
-  @Refreshable(dependsOn = "tdm.vehiclePipoRefreshInterval")
+  @Refreshable(dependsOn = {"tdm.vehiclePipoRefreshInterval", "tdm.vehiclePipoServiceEnabled"})
   private void configChanged() {
-    Integer updateInterval = _configurationService.getConfigurationValueAsInteger("tdm.vehiclePipoRefreshInterval", null);
-
-    if (updateInterval != null) {
-      setUpdateFrequency(updateInterval);
+    Integer updateIntervalSecs = _configurationService.getConfigurationValueAsInteger("tdm.vehiclePipoRefreshInterval", null);
+    _enabled = Boolean.parseBoolean(_configurationService.getConfigurationValueAsString("tdm.vehiclePipoServiceEnabled", "false"));
+    
+    if (updateIntervalSecs != null) {
+      setUpdateFrequency(updateIntervalSecs);
     }
   }
 
