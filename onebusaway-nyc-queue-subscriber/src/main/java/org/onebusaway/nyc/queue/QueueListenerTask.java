f5c6fa1f18d7f631c377ccd8a547da73139af259
Simon Jacobs <sjacobs@camsys.com>
2016-10-31 14:00:46 -0400

    Changes to QueueListenerTask from onebusaway-nyc-predictions: Ensure that ZMQ socket is not being used when queue listeners are shut down (for failover). Behavior difference between jzmq and jeromq.

--- onebusaway-nyc-queue-subscriber/src/main/java/org/onebusaway/nyc/queue/QueueListenerTask.java
+++ onebusaway-nyc-queue-subscriber/src/main/java/org/onebusaway/nyc/queue/QueueListenerTask.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -38,6 +40,9 @@ public abstract class QueueListenerTask {
 	protected ZMQ.Socket _socket = null;
 	protected ZMQ.Poller _poller = null;
 	protected int _countInterval = 10000;
+	
+  private ReadThread _readThread;
+  private Future<?> _readThreadFuture;
 
 	public abstract boolean processMessage(String address, byte[] buff) throws Exception;
 
@@ -78,6 +83,8 @@ public abstract class QueueListenerTask {
 
 		private ZMQ.Poller _zmqPoller = null;
 
+		private boolean _notifyShutdown = false;
+		
 		public ReadThread(ZMQ.Socket socket, ZMQ.Poller poller) {
 			_zmqSocket = socket;
 			_zmqPoller = poller;
@@ -87,7 +94,7 @@ public abstract class QueueListenerTask {
 		public void run() {
 		  _log.warn("ReadThread for queue " + getQueueName() + " starting");
 		  
-			while (!Thread.currentThread().isInterrupted()) {
+			while (!_notifyShutdown) {
 			  _zmqPoller.poll(1000 * 1000); // microseconds for 2.2, milliseconds for 3.0
 				if (_zmqPoller.pollin(0)) {
 
@@ -121,6 +128,10 @@ public abstract class QueueListenerTask {
 			}
 			_log.error("Thread loop Interrupted, exiting");
 		}
+		
+		public void notifyShutdown() {
+		  _notifyShutdown = true;
+		}
 	}
 
 	@PostConstruct
@@ -143,7 +154,7 @@ public abstract class QueueListenerTask {
 			return;
 		}
 	}
-
+	
 	// (re)-initialize ZMQ with the given args
 	protected synchronized void initializeQueue(String host, String queueName,
 			Integer port) throws InterruptedException {
@@ -155,10 +166,22 @@ public abstract class QueueListenerTask {
 		}
 
 		if (_socket != null) {
+		  
+		  // Notify read thread to shut down and wait.
+			_readThread.notifyShutdown();
+			try {
+			  _readThreadFuture.get(1, TimeUnit.SECONDS);
+			} catch (Exception e) {
+			  _log.error("Read thread did not complete cleanly: " + e.getMessage());
+      } 
+			
+			// Shut down service.
 			_executorService.shutdownNow();
-			Thread.sleep(1 * 1000);
-			_log.debug("_executorService.isTerminated="
+		  _executorService.awaitTermination(1, TimeUnit.SECONDS);
+			
+		  _log.debug("_executorService.isTerminated="
 					+ _executorService.isTerminated());
+
 			_socket.close();
 			_executorService = Executors.newFixedThreadPool(1);
 		}
@@ -169,7 +192,8 @@ public abstract class QueueListenerTask {
 		_socket.connect(bind);
 		_socket.subscribe(queueName.getBytes());
 
-		_executorService.execute(new ReadThread(_socket, _poller));
+		_readThread = new ReadThread(_socket, _poller);
+		_readThreadFuture = _executorService.submit(_readThread);
 
 		_log.warn("queue " + queueName + " is listening on " + bind);
 		_initialized = true;
@@ -177,7 +201,6 @@ public abstract class QueueListenerTask {
 	}
 
 	private class DNSCheckThread extends TimerTask {
-
 		@Override
 		public void run() {
 			try {
