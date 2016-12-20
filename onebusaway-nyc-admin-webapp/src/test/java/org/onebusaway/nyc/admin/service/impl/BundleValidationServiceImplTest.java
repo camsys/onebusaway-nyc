05750b634b9f6d092fb0f67d19c22b2073d66768
Lenny Caraballo <lcaraballo@gmail.com>
2016-12-20 13:30:25 -0500

    Adding tests for Gtfs Validation

--- onebusaway-nyc-admin-webapp/src/test/java/org/onebusaway/nyc/admin/service/impl/BundleValidationServiceImplTest.java
+++ onebusaway-nyc-admin-webapp/src/test/java/org/onebusaway/nyc/admin/service/impl/BundleValidationServiceImplTest.java
@@ -3,9 +3,12 @@ package org.onebusaway.nyc.admin.service.impl;
 import static org.junit.Assert.*;
 
 import org.onebusaway.nyc.admin.model.ServiceDateRange;
+import org.onebusaway.nyc.admin.service.bundle.GtfsValidationService;
 import org.onebusaway.nyc.admin.service.bundle.impl.BundleValidationServiceImpl;
+import org.onebusaway.nyc.admin.service.bundle.impl.JavaGtfsValidationServiceImpl;
+import org.onebusaway.nyc.admin.service.bundle.impl.PythonGtfsValidationServiceImpl;
 import org.onebusaway.nyc.admin.util.FileUtils;
-
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,15 +21,20 @@ import java.util.List;
 import java.util.Map;
 
 public class BundleValidationServiceImplTest {
+  
+  BundleValidationServiceImpl bundleValidationService;
+	
+  @Before
+  public void setup(){
+	  bundleValidationService = new BundleValidationServiceImpl();
+  }
 
   @Test
   public void testGetServiceDateRange() throws Exception {
-    BundleValidationServiceImpl impl = new BundleValidationServiceImpl();
-    // load zip file
-    InputStream input = this.getClass().getResourceAsStream(
-        "google_transit_staten_island.zip");
+	InputStream input = this.getClass().getResourceAsStream(
+		        "google_transit_staten_island.zip");
     assertNotNull(input);
-    List<ServiceDateRange> ranges = impl.getServiceDateRanges(input);
+    List<ServiceDateRange> ranges = bundleValidationService.getServiceDateRanges(input);
     assertNotNull(ranges);
     assertTrue(ranges.size() == 4);
     ServiceDateRange sdr0 = ranges.get(0);
@@ -42,13 +50,11 @@ public class BundleValidationServiceImplTest {
 
   @Test
   public void testCommonServiceDateRange() throws Exception {
-    BundleValidationServiceImpl impl = new BundleValidationServiceImpl();
-    // load zip file
-    InputStream input = this.getClass().getResourceAsStream(
-        "google_transit_staten_island.zip");
+	InputStream input = this.getClass().getResourceAsStream(
+		        "google_transit_staten_island.zip");
     assertNotNull(input);
-    List<ServiceDateRange> ranges = impl.getServiceDateRanges(input);
-    Map<String, List<ServiceDateRange>> map = impl.getServiceDateRangesByAgencyId(ranges);
+    List<ServiceDateRange> ranges = bundleValidationService.getServiceDateRanges(input);
+    Map<String, List<ServiceDateRange>> map = bundleValidationService.getServiceDateRangesByAgencyId(ranges);
     ServiceDateRange sdr0 = map.get("MTA NYCT").get(0);
     assertEquals("MTA NYCT", sdr0.getAgencyId());
     assertEquals(2012, sdr0.getStartDate().getYear());
@@ -62,14 +68,12 @@ public class BundleValidationServiceImplTest {
 
   @Test
   public void testCommonServiceDateRangeAcrossGTFS() throws Exception {
-    BundleValidationServiceImpl impl = new BundleValidationServiceImpl();
-    // load zip file
     ArrayList<InputStream> inputs = new ArrayList<InputStream>();
     inputs.add(this.getClass().getResourceAsStream(
         "google_transit_staten_island.zip"));
     inputs.add(this.getClass().getResourceAsStream(
         "google_transit_manhattan.zip"));
-    Map<String, List<ServiceDateRange>> map = impl.getServiceDateRangesAcrossAllGtfs(inputs);
+    Map<String, List<ServiceDateRange>> map = bundleValidationService.getServiceDateRangesAcrossAllGtfs(inputs);
     ServiceDateRange sdr0 = map.get("MTA NYCT").get(0);
 
     assertEquals(2012, sdr0.getStartDate().getYear());
@@ -81,9 +85,10 @@ public class BundleValidationServiceImplTest {
 
   }
 
-  //@Test
-  public void testValidateGtfs() throws Exception {
-    BundleValidationServiceImpl impl = new BundleValidationServiceImpl();
+  @Test
+  public void testPythonValidateGtfs() throws Exception {
+	GtfsValidationService validationService = new PythonGtfsValidationServiceImpl();
+	bundleValidationService.setGtfsValidationService(validationService);
     InputStream source = this.getClass().getResourceAsStream(
         "gtfs-m34.zip");
     assertNotNull(source);
@@ -93,13 +98,38 @@ public class BundleValidationServiceImplTest {
     String emptyFeedFilename = getTmpDir() + File.separatorChar + "good_feed.zip";
     assertTrue(new File(emptyFeedFilename).exists());
     String feedFilename = getTmpDir() + File.separatorChar + "feed.html";
-    int rc = impl.installAndValidateGtfs(emptyFeedFilename, 
+    int rc = bundleValidationService.installAndValidateGtfs(emptyFeedFilename, 
         feedFilename);
     // feedValidator exits with returnCode 1
     if (rc == 2) {
       throw new RuntimeException("please execute this command: 'sudo ln -s /usr/bin/python /usr/bin/python2.5'");
     }
-    assertEquals(1, rc);
+    assertEquals(0, rc);
+    File output = new File(feedFilename);
+    assertTrue(output.exists());
+    
+  }
+  
+  @Test
+  public void testJavaValidateGtfs() throws Exception {
+	GtfsValidationService validationService = new JavaGtfsValidationServiceImpl();
+	bundleValidationService.setGtfsValidationService(validationService);
+    InputStream source = this.getClass().getResourceAsStream(
+        "gtfs-m34.zip");
+    assertNotNull(source);
+    String goodFeedFilename = getTmpDir() + File.separatorChar + "good_feed.zip";
+    new FileUtils().copy(source, goodFeedFilename);
+    assertTrue(new File(goodFeedFilename).exists());
+    String emptyFeedFilename = getTmpDir() + File.separatorChar + "good_feed.zip";
+    assertTrue(new File(emptyFeedFilename).exists());
+    String feedFilename = getTmpDir() + File.separatorChar + "feed.html";
+    int rc = bundleValidationService.installAndValidateGtfs(emptyFeedFilename, 
+        feedFilename);
+    // feedValidator exits with returnCode 1
+    if (rc == 2) {
+      throw new RuntimeException("please confirm that you have Java 8 or greater installed'");
+    }
+    assertEquals(0, rc);
     File output = new File(feedFilename);
     assertTrue(output.exists());
     
