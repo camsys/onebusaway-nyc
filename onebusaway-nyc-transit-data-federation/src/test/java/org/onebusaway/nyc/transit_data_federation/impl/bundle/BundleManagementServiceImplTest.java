017fec3777cd28936a85b7feecf3665b82bf500d
Lenny Caraballo <lcaraballo@gmail.com>
2017-01-09 15:36:54 -0500

    Updated BundleSwitchFrequency to be configurable by the hour instead of by the minute. Updated BundleDiscoveryFrequency to offset its update frequency based on start of the hour instead of the server start up to make it consistent across instances. Updated the BundleManagementService tests to check the schedule logic

--- onebusaway-nyc-transit-data-federation/src/test/java/org/onebusaway/nyc/transit_data_federation/impl/bundle/BundleManagementServiceImplTest.java
+++ onebusaway-nyc-transit-data-federation/src/test/java/org/onebusaway/nyc/transit_data_federation/impl/bundle/BundleManagementServiceImplTest.java
@@ -7,7 +7,6 @@ import org.onebusaway.gtfs.model.calendar.ServiceDate;
 import org.onebusaway.nyc.transit_data_federation.model.bundle.BundleItem;
 import org.onebusaway.nyc.transit_data_federation.services.bundle.BundleStoreService;
 import org.onebusaway.utility.DateLibrary;
-
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -165,4 +165,79 @@ public class BundleManagementServiceImplTest extends BundleManagementServiceImpl
 
     assertEquals(super.getCurrentBundleMetadata().getId(), "test1");
   }
+  
+  @Test
+  public void testNextBundleSwitchTime(){
+	  
+	  super.setBundleSwitchFrequencyHour(1);
+	  
+	  Date currentDateTime = new Date(1483986900000L); // Mon, 09 Jan 2017 18:35:00 GMT
+	  Date expectedBundleSwitchTime = new Date(1483988401000L); // Mon, 09 Jan 2017 19:00:01 GMT
+	  Date actualBundleSwitchTime = super.getNextBundleSwitchTime(currentDateTime); 
+	  assertEquals(expectedBundleSwitchTime, actualBundleSwitchTime);
+	  
+	  // Right at the hour
+	  currentDateTime = new Date(1484078400000L); // Mon, 10 Jan 2017 20:00:00 GMT
+	  expectedBundleSwitchTime = new Date(1484082001000L); // Mon, 10 Jan 2017 21:00:01 GMT
+	  actualBundleSwitchTime = super.getNextBundleSwitchTime(currentDateTime); 
+	  assertEquals(expectedBundleSwitchTime, actualBundleSwitchTime);
+	  
+	  // Right at the end of the hour
+	  currentDateTime = new Date(1484164799000L); // Wed, 11 Jan 2017 19:59:59 GMT
+	  expectedBundleSwitchTime = new Date(1484164801000L); // Wed, 11 Jan 2017 20:00:01 GMT
+	  actualBundleSwitchTime = super.getNextBundleSwitchTime(currentDateTime); 
+	  assertEquals(expectedBundleSwitchTime, actualBundleSwitchTime);
+	  
+	  // 2 seconds before next hour
+	  currentDateTime = new Date(1484251198000L); // Thu, 12 Jan 2017 19:59:58 GMT
+	  expectedBundleSwitchTime = new Date(1484251201000L); // Thu, 12 Jan 2017 20:00:01 GMT
+	  actualBundleSwitchTime = super.getNextBundleSwitchTime(currentDateTime); 
+	  assertEquals(expectedBundleSwitchTime, actualBundleSwitchTime);
+  }
+  
+  @Test
+  public void testNextBundleDiscoveryTime(){
+	  
+	  // Test 15 min Frequency 
+	  super.setBundleDiscoveryFrequencyMin(15);
+	  
+	  Date currentDateTime = new Date(1483986900000L); // Mon, 09 Jan 2017 18:35:00 GMT
+	  Date expectedBundleDiscoveryTime = new Date(1483987500000L); // Mon, 09 Jan 2017 18:45:00 GMT
+	  Date actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+
+	  currentDateTime = new Date(1483984860000L); // Mon, 09 Jan 2017 18:01:00 GMT
+	  expectedBundleDiscoveryTime = new Date(1483985700000L); // Mon, 09 Jan 2017 18:15:00 GMT
+	  actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+	  
+	  currentDateTime = new Date(1483988280000L); // Mon, 09 Jan 2017 18:58:00 GMT
+	  expectedBundleDiscoveryTime = new Date(1483988400000L); // Mon, 09 Jan 2017 19:00:00 GMT
+	  actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+	  
+	  
+	  // Test 5 min Frequency 
+	  super.setBundleDiscoveryFrequencyMin(5);
+	  
+	  currentDateTime = new Date(1483986540000L); // Mon, 09 Jan 2017 18:29:00 GMT
+	  expectedBundleDiscoveryTime = new Date(1483986600000L); // Mon, 09 Jan 2017 18:30:00 GMT
+	  actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+	  
+	  currentDateTime = new Date(1483986600000L); // Mon, 09 Jan 2017 18:30:00 GMT
+	  expectedBundleDiscoveryTime = new Date(1483986900000L); // Mon, 09 Jan 2017 18:35:00 GMT
+	  actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+	  
+	  
+	  // Test 29 min Frequency
+	  super.setBundleDiscoveryFrequencyMin(29);
+	  
+	  currentDateTime = new Date(1483988280000L); // Mon, 09 Jan 2017 18:58:00 GMT
+	  expectedBundleDiscoveryTime = new Date(1483990020000L); // Mon, 09 Jan 2017 19:27:00 GMT
+	  actualBundleDiscoveryTime = super.getNextBundleDiscoveryTime(currentDateTime); 
+	  assertEquals(expectedBundleDiscoveryTime, actualBundleDiscoveryTime);
+  }
+  
 }
