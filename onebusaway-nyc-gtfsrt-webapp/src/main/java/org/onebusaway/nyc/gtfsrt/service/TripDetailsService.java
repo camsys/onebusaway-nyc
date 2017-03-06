abb864bc752e4e3f6056fce05964745449e4940a
Simon Jacobs <sjacobs@camsys.com>
2017-03-06 11:54:35 -0500

    Remove unneccessary TripDetailsService

--- onebusaway-nyc-gtfsrt-webapp/src/main/java/org/onebusaway/nyc/gtfsrt/service/TripDetailsService.java
+++
@@ -1,8 +0,0 @@
-package org.onebusaway.nyc.gtfsrt.service;
-
-import org.onebusaway.transit_data.model.VehicleStatusBean;
-import org.onebusaway.transit_data.model.trips.TripDetailsBean;
-
-public interface TripDetailsService {
-    TripDetailsBean getTripDetailsForVehicleStatus(VehicleStatusBean bean);
-}
