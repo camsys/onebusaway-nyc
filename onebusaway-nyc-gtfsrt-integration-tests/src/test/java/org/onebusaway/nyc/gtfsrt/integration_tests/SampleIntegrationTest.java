d3d63ee03d34076e8dc66980be107901cee3def9
sheldonabrown <sheldonb@gmail.com>
2017-04-14 13:43:38 -0400

    adding integration test that has succesfull gtfsrt response.
    Still need to compare fields...

--- onebusaway-nyc-gtfsrt-integration-tests/src/test/java/org/onebusaway/nyc/gtfsrt/integration_tests/SampleIntegrationTest.java
+++ onebusaway-nyc-gtfsrt-integration-tests/src/test/java/org/onebusaway/nyc/gtfsrt/integration_tests/SampleIntegrationTest.java
@@ -5,6 +5,6 @@ package org.onebusaway.nyc.gtfsrt.integration_tests;
  */
 public class SampleIntegrationTest extends AbstractInputRunner {
     public SampleIntegrationTest() throws Exception {
-        super("20170412_7198", "2017April_Prod_rt_test", "2017-04-12T11:00:00EDT");
+        super("20170412_7198", "2017April_Prod_rt_test", "2017-04-12T11:05:00EDT");
     }
 }
