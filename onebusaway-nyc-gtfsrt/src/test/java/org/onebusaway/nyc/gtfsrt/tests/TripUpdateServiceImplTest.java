33acafe3dfee004fc0b5a9d9e2a4b745473bcb0c
Simon Jacobs <sjacobs@camsys.com>
2017-04-17 11:13:58 -0400

    trip update service coverage test

---
+++ onebusaway-nyc-gtfsrt/src/test/java/org/onebusaway/nyc/gtfsrt/tests/TripUpdateServiceImplTest.java
@@ -0,0 +1,125 @@
+package org.onebusaway.nyc.gtfsrt.tests;
+
+import com.google.transit.realtime.GtfsRealtime.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.onebusaway.nyc.gtfsrt.impl.TripUpdateServiceImpl;
+import org.onebusaway.nyc.gtfsrt.service.TripUpdateFeedBuilder;
+import org.onebusaway.nyc.transit_data.services.NycTransitDataService;
+import org.onebusaway.realtime.api.TimepointPredictionRecord;
+import org.onebusaway.transit_data.model.VehicleStatusBean;
+import org.onebusaway.transit_data.model.blocks.BlockConfigurationBean;
+import org.onebusaway.transit_data.model.blocks.BlockInstanceBean;
+import org.onebusaway.transit_data.model.blocks.BlockTripBean;
+import org.onebusaway.transit_data.model.trips.TripBean;
+import org.onebusaway.transit_data.model.trips.TripStatusBean;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TripUpdateServiceImplTest {
+  private TripUpdateServiceImpl service;
+  private NycTransitDataService tds;
+
+  @Before
+  public void setup() {
+    TripUpdateFeedBuilder feedBuilder = new TripUpdateFeedBuilder() {
+      @Override
+      public TripUpdate.Builder makeTripUpdate(TripBean trip, VehicleStatusBean vehicle, List<TimepointPredictionRecord> records) {
+        return TripUpdate.newBuilder().setTrip(TripDescriptor.newBuilder().setTripId(trip.getId()));
+      }
+    };
+
+    tds = mock(NycTransitDataService.class);
+
+    service = new TripUpdateServiceImpl();
+    service.setFeedBuilder(feedBuilder);
+    service.setTransitDataService(tds);
+    service.setAgencyId("agency");
+  }
+
+  @Test
+  public void testRegularTripUpdate() {
+    VehicleStatusBean vsb = vehicleStatus("vehicle", "block", 0);
+    BlockInstanceBean block = blockInstance(blockTrip("trip"));
+    when(tds.getAllVehiclesForAgency("agency", 0)).thenReturn(UnitTestSupport.listBean(vsb));
+    when(tds.getBlockInstance("block", 0)).thenReturn(block);
+    List<FeedEntity.Builder> ret = service.getEntities(0);
+    assertEquals(1, ret.size());
+    FeedEntity.Builder fe = ret.get(0);
+    assertTrip("trip", fe);
+  }
+
+  @Test
+  public void testTripUpdateMultipleTripsOnBlock() {
+    VehicleStatusBean vsb = vehicleStatus("vehicle", "block", 0);
+    BlockInstanceBean block = blockInstance(blockTrip("trip0"), blockTrip("trip1"));
+    when(tds.getAllVehiclesForAgency("agency", 0)).thenReturn(UnitTestSupport.listBean(vsb));
+    when(tds.getBlockInstance("block", 0)).thenReturn(block);
+    List<FeedEntity.Builder> ret = service.getEntities(0);
+    assertEquals(2, ret.size());
+    Collections.sort(ret, ALPHABETIC_BY_ID);
+    assertTrip("trip0", ret.get(0));
+    assertTrip("trip1", ret.get(1));
+  }
+
+  @Test
+  public void test2ndTripOnBlock() {
+    VehicleStatusBean vsb = vehicleStatus("vehicle", "block", 1);
+    BlockInstanceBean block = blockInstance(blockTrip("trip0"), blockTrip("trip1"));
+    when(tds.getAllVehiclesForAgency("agency", 0)).thenReturn(UnitTestSupport.listBean(vsb));
+    when(tds.getBlockInstance("block", 0)).thenReturn(block);
+    List<FeedEntity.Builder> ret = service.getEntities(0);
+    assertEquals(1, ret.size());
+    assertTrip("trip1", ret.get(0));
+  }
+
+  private BlockTripBean blockTrip(String tripId) {
+    BlockTripBean btb = new BlockTripBean();
+    btb.setTrip(new TripBean());
+    btb.getTrip().setId(tripId);
+    return btb;
+  }
+
+  private BlockInstanceBean blockInstance(BlockTripBean... trips) {
+    BlockInstanceBean block = new BlockInstanceBean();
+    block.setBlockConfiguration(new BlockConfigurationBean());
+    block.getBlockConfiguration().setTrips(Arrays.asList(trips));
+    return block;
+  }
+
+  private VehicleStatusBean vehicleStatus(String vehicle, String block, int tripSequence) {
+    VehicleStatusBean vsb = new VehicleStatusBean();
+    vsb.setVehicleId(vehicle);
+    vsb.setTripStatus(new TripStatusBean());
+    vsb.getTripStatus().setBlockTripSequence(tripSequence);
+    vsb.setTrip(new TripBean());
+    vsb.getTrip().setBlockId(block);
+    return vsb;
+  }
+
+  private void assertTrip(String trip, FeedEntityOrBuilder fe) {
+    assertTrue(fe.hasTripUpdate());
+    assertFalse(fe.hasVehicle());
+    assertFalse(fe.hasAlert());
+
+    assertEquals(trip, fe.getTripUpdate().getTrip().getTripId());
+    // convention:
+    assertEquals(trip, fe.getId());
+  }
+
+  private static final Comparator<FeedEntityOrBuilder> ALPHABETIC_BY_ID = new Comparator<FeedEntityOrBuilder>() {
+    @Override
+    public int compare(FeedEntityOrBuilder o1, FeedEntityOrBuilder o2) {
+      return o1.getId().compareTo(o2.getId());
+    }
+  };
+}
