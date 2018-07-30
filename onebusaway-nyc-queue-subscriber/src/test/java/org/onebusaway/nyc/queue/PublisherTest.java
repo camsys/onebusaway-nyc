e30a4fb619976a188958ee34b336f788adbe65c6
Unknown <lennycaraballo@gmail.com>
2018-07-30 00:21:14 -0400

    Fix to check if rmc data exists before extracting rmc dates

--- onebusaway-nyc-queue-subscriber/src/test/java/org/onebusaway/nyc/queue/PublisherTest.java
+++ onebusaway-nyc-queue-subscriber/src/test/java/org/onebusaway/nyc/queue/PublisherTest.java
@@ -42,9 +42,7 @@ import java.util.Date;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class PublisherTest {
@@ -166,6 +164,20 @@ public class PublisherTest {
       String envelope = p.wrap(buff);
       assertEquals(null, envelope);
   }
+
+    @Test
+    public void testMissingRmc(){
+        Publisher p = new Publisher("topic") {
+            String generateUUID() {
+                return "foo";
+            }
+            long getTimeReceived() {
+                return 1234567;
+            }
+        };
+        String ccmessage = "{\"CcLocationReport\":{\"request-id\":1008,\"vehicle\":{\"vehicle-id\":242,\"agency-id\":2008,\"agencydesignator\":\"MTA NYCT\"},\"status-info\":0,\"time-reported\":\"2018-07-18T03:58:58.0-00:00\",\"latitude\":40616413,\"longitude\":-74031067,\"direction\":{\"deg\":196.6},\"speed\":30,\"manufacturer-data\":\"BMV54616\",\"operatorID\":{\"operator-id\":0,\"designator\":\"460003\"},\"runID\":{\"run-id\":0,\"designator\":\"49\"},\"destSignCode\":12,\"routeID\":{\"route-id\":0,\"route-designator\":\"8\"},\"localCcLocationReport\":{\"NMEA\":{\"sentence\":[\"$GPGGA,035857.677,4036.98481,N,07401.86405,W,1,09,1.07,00037.6,M,-034.3,M,,*60\"]},\"vehiclePowerState\":1}}}";
+        assertNull(p.getRmcData(new StringBuffer(ccmessage)));
+    }
 
     @Test
     public void testGetRmcDateTime() throws ParseException {
