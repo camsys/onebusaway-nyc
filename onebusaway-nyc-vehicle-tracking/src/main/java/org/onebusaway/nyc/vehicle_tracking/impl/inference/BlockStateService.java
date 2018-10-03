5658c26ce9e4f130d9de649427fb8ff56263feec
Simon Jacobs <sidaja@gmail.com>
2018-10-03 13:53:40 -0400

    GTFS-131 refresh BlockStateService after GTFS service change (holds state on blocks)

--- onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/BlockStateService.java
+++ onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/impl/inference/BlockStateService.java
@@ -168,7 +168,8 @@ public class BlockStateService {
   @PostConstruct
   @Refreshable(dependsOn = {
       RefreshableResources.TRANSIT_GRAPH, 
-      RefreshableResources.NARRATIVE_DATA})
+      RefreshableResources.NARRATIVE_DATA,
+      RefreshableResources.BLOCK_SHAPE_DATA})
   public void setup() throws IOException, ClassNotFoundException {
     buildShapeSpatialIndex();
   }
