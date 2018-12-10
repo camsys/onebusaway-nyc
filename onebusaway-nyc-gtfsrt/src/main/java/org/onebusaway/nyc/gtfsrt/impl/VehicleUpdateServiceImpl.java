d7ce67d7c733339eb04b8864dba6080e13f96ee8
Unknown <lennycaraballo@gmail.com>
2018-12-10 16:22:12 -0500

    Changes to Gtfsrt to only show vehicles with active trips

--- onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/VehicleUpdateServiceImpl.java
+++ onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/VehicleUpdateServiceImpl.java
@@ -17,15 +17,21 @@ package org.onebusaway.nyc.gtfsrt.impl;
 
 import com.google.transit.realtime.GtfsRealtime.*;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.nyc.gtfsrt.service.VehicleUpdateFeedBuilder;
+import org.onebusaway.nyc.presentation.service.realtime.PresentationService;
 import org.onebusaway.nyc.transit_data.services.NycTransitDataService;
 import org.onebusaway.realtime.api.OccupancyStatus;
 import org.onebusaway.realtime.api.VehicleOccupancyRecord;
 import org.onebusaway.transit_data.model.VehicleStatusBean;
 import org.onebusaway.transit_data.model.realtime.VehicleLocationRecordBean;
+import org.onebusaway.transit_data.model.trips.TripDetailsBean;
+import org.onebusaway.transit_data.model.trips.TripDetailsInclusionBean;
+import org.onebusaway.transit_data.model.trips.TripForVehicleQueryBean;
 import org.onebusaway.transit_data.model.trips.TripStatusBean;
 
+import org.onebusaway.transit_data.services.TransitDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +39,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -43,6 +50,7 @@ public class VehicleUpdateServiceImpl extends AbstractFeedMessageService {
 
     private VehicleUpdateFeedBuilder _feedBuilder;
     private NycTransitDataService _transitDataService;
+    private PresentationService _presentationService;
 
     private static final Logger _log = LoggerFactory.getLogger(VehicleUpdateServiceImpl.class);
 
@@ -56,9 +64,14 @@ public class VehicleUpdateServiceImpl extends AbstractFeedMessageService {
         _transitDataService = transitDataService;
     }
 
+    @Autowired
+    public void setPresentationService(PresentationService presentationService) {
+        _presentationService = presentationService;
+    }
+
     @Override
     public List<FeedEntity.Builder> getEntities(long time) {
-        Collection<VehicleStatusBean> vehicles = getAllVehicles(_transitDataService, time);
+        Collection<VehicleStatusBean> vehicles = getAllVehicles(_transitDataService, _presentationService, time);
 
         List<FeedEntity.Builder> entities = new ArrayList<FeedEntity.Builder>();
 
@@ -66,7 +79,7 @@ public class VehicleUpdateServiceImpl extends AbstractFeedMessageService {
 
         for (VehicleStatusBean vehicle : vehicles) {
 
-            if (vehicle.getTrip() == null) {
+            if (!includeVehicle(_transitDataService, _presentationService, vehicle, time)) {
                 continue;
             }
 
@@ -75,7 +88,7 @@ public class VehicleUpdateServiceImpl extends AbstractFeedMessageService {
                 nMissing++;
                 continue;
             }
-            
+
             OccupancyStatus occupancy = getOccupancyStatus(vehicle);
             VehiclePosition.Builder pos = _feedBuilder.makeVehicleUpdate(vehicle, vlr, occupancy);
 
@@ -90,7 +103,7 @@ public class VehicleUpdateServiceImpl extends AbstractFeedMessageService {
 
         return entities;
     }
-    
+
     private OccupancyStatus getOccupancyStatus(VehicleStatusBean vehicleStatus){
       TripStatusBean tripStatus = vehicleStatus.getTripStatus();
       
