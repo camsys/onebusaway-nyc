155623cb094085efc47730b8a5619c21fe3cd34f
carabalb <lennycaraballo@gmail.com>
2018-12-19 11:11:52 -0500

    Adding presentation service dependencies for gtfsrt

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/NycTransitDataServiceImpl.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/NycTransitDataServiceImpl.java
@@ -91,11 +91,13 @@ import org.onebusaway.transit_data_federation.services.revenue.RevenueSearchServ
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-@Component
+@Primary
+@Component(value = "nycTransitDataServiceImpl")
 class NycTransitDataServiceImpl implements NycTransitDataService {
 	
 	private static Logger _log = LoggerFactory.getLogger(NycTransitDataServiceImpl.class);
