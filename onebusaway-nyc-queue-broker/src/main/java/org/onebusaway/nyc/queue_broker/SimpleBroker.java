0ada8f5ae1e79cd8efbbdb6e8edc590ca0bd0eae
Lenny Caraballo <lennycaraballo@gmail.com>
2019-03-19 14:55:01 -0400

    Revert "Setting High Water Mark for ZeroMQ to limit publisher buffer growth"
    This reverts commit ceba7aaafe385c476674662aeac59cad278a65a0.

--- onebusaway-nyc-queue-broker/src/main/java/org/onebusaway/nyc/queue_broker/SimpleBroker.java
+++ onebusaway-nyc-queue-broker/src/main/java/org/onebusaway/nyc/queue_broker/SimpleBroker.java
@@ -17,7 +17,6 @@ public class SimpleBroker {
 
   private static final int DEFAULT_IN_PORT = 5566;
   private static final int DEFAULT_OUT_PORT = 5567;
-  private static final int HWM_VALUE = 10000; // High Water Mark
   private ExecutorService _executorService = null;
   private int inPort;
   private int outPort;
@@ -55,10 +54,6 @@ public class SimpleBroker {
     subscriber.subscribe(new byte[0]); // was inTopic.getBytes()
 
     ZMQ.Socket publisher = context.socket(ZMQ.PUB);
-
-    //Set to prevent broker memory from growing indefinitely if client falls behind
-    publisher.setHWM(HWM_VALUE);
-
     String outBind = "tcp://*:" + outPort;
 
     logger.info("publishing to queue at " + outBind);
