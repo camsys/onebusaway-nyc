ed25286dba3c6286864db2cef11f5304acce486d
Nicolas Beiro LeBeau <nlebeau@camsys.com>
2019-12-18 11:18:47 -0500

    init with temporary ugly SVG bus icons

--- onebusaway-nyc-webapp/src/main/webapp/js/oba/map/RouteMap.js
+++ onebusaway-nyc-webapp/src/main/webapp/js/oba/map/RouteMap.js
@@ -200,7 +200,7 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 		var agencyId = routeIdParts[0];
 		var routeIdWithoutAgency = routeIdParts[1];
 		
-		var params = { OperatorRef: agencyId, LineRef: routeIdWithoutAgency };		
+		var params = { OperatorRef: agencyId, LineRef: routeIdWithoutAgency };
 
 		if(OBA.Config.time !== null) {
 			params.time = OBA.Config.time;
@@ -209,7 +209,7 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 		if(typeof siriVMRequestsByRouteId[routeId] !== 'undefined' && siriVMRequestsByRouteId[routeId] !== null) {
 			siriVMRequestsByRouteId[routeId].abort();
 		}
-		siriVMRequestsByRouteId[routeId] = jQuery.getJSON(OBA.Config.siriVMUrl + "&callback=?", params, 
+		siriVMRequestsByRouteId[routeId] = jQuery.getJSON(OBA.Config.siriVMUrl + "&callback=?", params,
 		function(json) {
 			// service alerts
 			if(typeof serviceAlertCallbackFn === 'function') {
@@ -221,6 +221,7 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 			
 			// service delivery
 			var vehiclesByIdInResponse = {};
+			var isSpooking = false;
 			jQuery.each(json.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity, function(_, activity) {
 				var latitude = activity.MonitoredVehicleJourney.VehicleLocation.Latitude;
 				var longitude = activity.MonitoredVehicleJourney.VehicleLocation.Longitude;
@@ -231,25 +232,30 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 				var vehicleId = activity.MonitoredVehicleJourney.VehicleRef;
 				var vehicleIdParts = vehicleId.split("_");
 				var vehicleIdWithoutAgency = vehicleIdParts[1];
+				var markers = vehiclesById[vehicleId];
 				var marker = vehiclesById[vehicleId];
 				
 				// has route been removed while in the process of updating?
 				if(typeof vehiclesByRoute[routeId] === 'undefined') {
 					return false;
 				}
-				
+				isSpooking = false;
+				if(activity.MonitoredVehicleJourney.ProgressStatus == "spooking") {
+					isSpooking = true;
+				}
 				// create marker if it doesn't exist				
-				if(typeof marker === 'undefined' || marker === null) {
+				if(typeof markers === 'undefined' || markers === null || markers[0] === null) {
 					var markerOptions = {
-						zIndex: 3,
+						zIndex: 4,
 						map: map,
 						title: "Vehicle " + vehicleIdWithoutAgency + ", " + routeName + " to " + headsign,
 						vehicleId: vehicleId,
-						routeId: routeId
+						routeId: routeId,
+						// optimized: false
 					};
 
 					marker = new google.maps.Marker(markerOptions);
-			        
+
 			    	google.maps.event.addListener(marker, "click", function(mouseEvent) {
 			    		OBA.Config.analyticsFunction("Vehicle Marker Click", vehicleIdWithoutAgency);
 
@@ -257,31 +263,120 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 			    				{ OperatorRef: agencyId, VehicleRef: vehicleIdWithoutAgency, MaximumNumberOfCallsOnwards: "3", VehicleMonitoringDetailLevel: "calls" }, 
 			    				OBA.Popups.getVehicleContentForResponse, null);
 			    	});
+
+					var icon = {
+						path: "M3.9455914575088977,60.06508343142002h0.06399999999999206l0.014000000000010004,6.942000000000007c-0.04600000000000648,0.3469999999999942,-0.07800000000000251,0.6979999999999933,-0.07800000000000251,1.058000000000007v31.999999999999986c0,4.419000000000011,3.5810000000000026,8,8,8v8c0,4.419000000000011,3.581000000000003,8,8,8h8c4.418999999999983,0,8,-3.5810000000000173,8,-8v-8h48v8c0,4.418999999999983,3.5810000000000173,8,8,8h8c4.418999999999983,0,8,-3.5810000000000173,8,-8v-8c4.418999999999983,0,8,-3.5810000000000173,8,-8v-40c4.418999999999983,0,8,-3.581000000000003,8,-8v-8c0,-4.418999999999997,-3.5810000000000173,-8,-8,-8v-16c0,-13.254999999999995,-10.745000000000005,-24,-24,-24h-64c-13.254999999999995,0,-24,10.745000000000005,-24,24v8l0.015999999999997794,8h-0.015999999999997794c-4.418999999999997,0,-8,3.581000000000003,-8,8v8c0,4.418999999999997,3.581000000000003,8,8,8ZM35.9455914575089,84.14508343142002c0,4.373999999999995,-3.5459999999999923,7.920000000000002,-7.920000000000016,7.920000000000002h-8.159999999999975c-4.3740000000000165,0,-7.920000000000009,-3.5460000000000065,-7.920000000000009,-7.920000000000002v-0.1599999999999966c0,-4.373999999999995,3.5459999999999923,-7.920000000000002,7.920000000000009,-7.920000000000002h8.159999999999975c4.374000000000024,0,7.920000000000016,3.5460000000000065,7.920000000000016,7.920000000000002v0.1599999999999966ZM107.9455914575089,84.14508343142002c0,4.373999999999995,-3.5459999999999923,7.920000000000002,-7.920000000000016,7.920000000000002h-8.159999999999968c-4.374000000000024,0,-7.920000000000016,-3.5460000000000065,-7.920000000000016,-7.920000000000002v-0.1599999999999966c0,-4.373999999999995,3.5459999999999923,-7.920000000000002,7.920000000000016,-7.920000000000002h8.159999999999968c4.374000000000024,0,7.920000000000016,3.5460000000000065,7.920000000000016,7.920000000000002v0.1599999999999966ZM11.945591457508897,20.06508343142002c0,-8.835999999999999,7.162999999999997,-16,16,-16h64c8.836999999999989,0,16,7.162999999999997,16,16l-0.06700000000000728,40h-95.93299999999999v-40Z",
+						size: new google.maps.Size(50, 50),
+						// origin: new google.maps.Point(0,0),
+						anchor: new google.maps.Point(60, 60),
+						strokeColor: "blue",
+						fillColor: "blue",
+						fillOpacity: 1,
+						// scaledSize: new google.maps.Size(51, 51),
+						scale: 0.15
+					}
+
+					if(isSpooking) {
+						icon.strokeColor = "red";
+						icon.fillColor = "red";
+						marker.setTitle("Spooking Bus"); // TODO: Need a title for spooking buses
+						marker.setOpacity(0.6);
+					}
+
+					marker.setIcon(icon);
+
+
+
+					if(typeof arrowMarker === 'undefined' || arrowMarker === null) {
+						var arrowMarkerOptions = {
+							position: marker.getPosition(),
+							zIndex:3,
+							map:map,
+							// optimized:false
+						}
+						var arrowMarker = new google.maps.Marker(arrowMarkerOptions);
+
+						//var radOrientation = (orientation*Math.PI)/180; //convert to radians
+						var anchor = new google.maps.Point(0, -12);//-5*Math.cos(radOrientation), 5*Math.sin(radOrientation)); // don't actually have to do trig. it translates after rotating.
+
+
+						var arrowIcon = {
+							path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,//"M3.75,0l-7.32758638177701,-4.0086206970968945h-0.17241361822299028l1.25,4.0086206970968945l-1.25,3.75h0.04310326967453193Z",//"M2.7012712033923014,-1.2076270991240101l-7.47881345863967,-3.8771186420130563h-0.021186541360330935l1.2500000000000013,3.8771186420130572l-1.2500000000000013,3.749999999999999h0.021186541360327382Z",//google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,//
+							scale: 2,
+							anchor: anchor,
+							rotation: orientation,
+							strokeWeight: 0.5,
+							strokeColor: "black",
+							strokeOpacity: 1,
+							fillColor: "blue",
+							fillOpacity: 1
+
+						};
+
+						arrowMarker.setIcon(arrowIcon);
+						arrowMarker.setPosition(position);
+						arrowMarker.bindTo("position", marker);
+					}
+				} else {
+					marker = markers[0];
+					arrowMarker = markers[1];
+					arrowIcon = arrowMarker.getIcon();
 				}
+
 
 				// icon
-				var orientationAngle = "unknown";
-				if(orientation !== null && orientation !== 'NaN') {
-					orientationAngle = Math.floor(orientation / 5) * 5;
-				}
+				// var orientationAngle = "unknown";
+				// if(orientation !== null && orientation !== 'NaN') {
+				// 	orientationAngle = Math.floor(orientation / 5) * 5;
+				// }
 					
-				var icon = new google.maps.MarkerImage("img/vehicle/vehicle-" + orientationAngle + ".png",
-						new google.maps.Size(51, 51),
-						new google.maps.Point(0,0),
-						new google.maps.Point(25, 25));
+				// var icon = new google.maps.MarkerImage("img/vehicle/vehicle-unknown.png",
+				// 		new google.maps.Size(51, 51),
+				// 		new google.maps.Point(0,0),
+				// 		new google.maps.Point(25, 25));
 
-				marker.setIcon(icon);
+				// var overlay = new google.maps.OverlayView();
+				// overlay.draw = function () {
+				// 	this.getPanes().markerLayer.id = 'markerLayer';
+				// 	console.log(this.getPanes());
+				// };
+				// overlay.setMap(map);
 
 				// position
 				var position = new google.maps.LatLng(latitude, longitude);
 				marker.setPosition(position);
+
+
+
+				orientation = 360-((orientation + 90) % 360);
+				arrowIcon.rotation = orientation;
+
+
+				if (isSpooking) {
+					arrowIcon.strokeOpacity = 0.4;
+					arrowIcon.fillColor = "red";
+					arrowIcon.fillOpacity = 0.5;
+				} else if(activity.MonitoredVehicleJourney.ProgressStatus == "layover") {
+					arrowIcon.strokeOpacity = 1;
+					arrowIcon.fillColor = "yellow";
+					arrowIcon.fillOpacity = 1;
+				} else {
+					arrowIcon.strokeOpacity = 1;
+					arrowIcon.fillColor = "blue";
+					arrowIcon.fillOpacity = 1;
+				}
+
+				arrowMarker.setIcon(arrowIcon);
+				arrowMarker.setPosition(position);
+				arrowMarker.bindTo("position", marker);
+
 							    	
 				// (mark that this vehicle is still in the response)
 				vehiclesByIdInResponse[vehicleId] = true;
 
 				// maps used to keep track of marker
-				vehiclesByRoute[routeId][vehicleId] = marker;
-				vehiclesById[vehicleId] = marker; 
+				vehiclesByRoute[routeId][vehicleId] = [marker, arrowMarker];
+				vehiclesById[vehicleId] = [marker, arrowMarker];
 			});
 			
 			// remove vehicles from map that are no longer in the response, for all routes in the query
@@ -295,11 +390,26 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 					}
 					
 					vehicleOnMap.setMap(null);
-					delete vehiclesById[vehicleOnMap_vehicleId];
-					delete vehiclesByRoute[vehicleOnMap_routeId][vehicleOnMap_vehicleId];
+
+					delete vehiclesById[vehicleOnMap_vehicleId][0];
+					delete vehiclesById[vehicleOnMap_vehicleId][1];
+					delete vehiclesByRoute[vehicleOnMap_routeId][vehicleOnMap_vehicleId][0];
+					delete vehiclesByRoute[vehicleOnMap_routeId][vehicleOnMap_vehicleId][1];
 				}
 			});
 		});
+
+		// ghost bus overlay
+		// if(isSpooking) {
+		// 	var overlay = new google.maps.OverlayView();
+		// 	overlay.draw = function(){
+		// 		this.getPanes().markerLayer.id='markerLayer';
+		// 	};
+		// 	overlay.setMap(map);
+		// }
+
+
+
 	}
 	
 	function removeVehicles(routeId) {
@@ -307,10 +417,11 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 			var vehicles = vehiclesByRoute[routeId];
 			delete vehiclesByRoute[routeId];
 			
-			jQuery.each(vehicles, function(_, marker) {
-				var vehicleId = marker.vehicleId;
+			jQuery.each(vehicles, function(_, markers) {
+				var vehicleId = markers[0].vehicleId;
 				
-				marker.setMap(null);
+				markers[0].setMap(null);
+				markers[1].setMap(null);
 				delete vehiclesById[vehicleId];
 			});
 		}
@@ -346,7 +457,7 @@ OBA.RouteMap = function(mapNode, initCallbackFn, serviceAlertCallbackFn) {
 		}
 		highlightedStop = null;
 	}
-		
+
 	//////////////////// CONSTRUCTOR /////////////////////
 
 	map = new OBA.GoogleMapWrapper(document.getElementById("map"));
