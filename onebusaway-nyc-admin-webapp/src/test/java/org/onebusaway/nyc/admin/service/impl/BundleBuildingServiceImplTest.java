27d6534849f7b4593f3377560a012a75cb179a0c
caylasavitzky <caysavitzky@gmail.com>
2020-01-30 16:48:14 -0500

    Added more extensive bundle building test

--- onebusaway-nyc-admin-webapp/src/test/java/org/onebusaway/nyc/admin/service/impl/BundleBuildingServiceImplTest.java
+++ onebusaway-nyc-admin-webapp/src/test/java/org/onebusaway/nyc/admin/service/impl/BundleBuildingServiceImplTest.java
@@ -2,6 +2,7 @@ package org.onebusaway.nyc.admin.service.impl;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.onebusaway.nyc.admin.model.BundleBuildRequest;
 import org.onebusaway.nyc.admin.model.BundleBuildResponse;
 import org.onebusaway.nyc.admin.service.FileService;
@@ -75,7 +76,24 @@ public class BundleBuildingServiceImplTest {
           list.add("stif-m34.zip");
         } else if (directory.equals("test/config")) {
           // do nothing
-        } else {
+        } else if (directory.equals("extensiveTest/gtfs_latest")) {
+          list.add("google_transit_mta_agency.zip");
+          list.add("google_transit_queens.zip");
+          list.add("google_transit_staten_island.zip");
+          list.add("google_transit_bronx.zip");
+          list.add("google_transit_brooklyn.zip");
+          list.add("google_transit_manhattan.zip");
+        } else if (directory.equals("extensiveTest/stif_latest")) {
+          list.add("STIF_MTABC_B9_AsAssigned_Rte_Included_v2-v1.zip");
+          list.add("STIF_SURFACE_BX_2019-04-28_REV2019-03-19_1348211.zip");
+          list.add("STIF_SURFACE_B_2019-04-28_REV2019-03-22_1507259-v2.zip");
+          list.add("STIF_SURFACE_M_2019-04-28_REV2019-04-05_1059035.zip");
+          list.add("STIF_SURFACE_Q_2019-04-28_REV2019-05-28_1348461-v2.zip");
+          list.add("STIF_SURFACE_S_2019-04-28_REV2019-05-01_0929286.zip");
+        } else if (directory.equals("extensiveTest/config")) {
+          // do nothing
+        }
+        else{
           list.add("empty");
         }
         return list;
@@ -90,6 +108,8 @@ public class BundleBuildingServiceImplTest {
               "gtfs-m34.zip");
         } else if (key.equals("stif-m34.zip")) {
           source = this.getClass().getResourceAsStream("stif-m34.zip");
+        } else {
+          source = this.getClass().getResourceAsStream(key);
         }
         String filename = tmpDir + File.separator + key;
         new FileUtils().copy(source, filename);
@@ -123,29 +143,44 @@ public class BundleBuildingServiceImplTest {
   }
 
   @Test
-  public void testBuild() {
-    String bundleDir = "test";
+  public void testBuild(){
+    testBuild(1);
+  }
+
+  @Ignore
+  @Test
+  public void extensiveTestBuild(){
+    testBuild(2);
+  }
+
+  public void testBuild(int mode) {
+    String bundleDir = (mode == 1) ? "test" : "extensiveTest";
+
     String tmpDir = new FileUtils().createTmpDirectory();
 
     BundleBuildRequest request = new BundleBuildRequest();
     request.setBundleDirectory(bundleDir);
     request.setBundleName("testname");
     request.setTmpDirectory(tmpDir);
-    request.setBundleStartDate("2012-04-08");
-    request.setBundleEndDate("2012-07-07");
+    String startDate = (mode == 1) ? "2012-04-08" : "2020-01-08";
+    request.setBundleStartDate(startDate);
+    String endDate = (mode == 1) ? "2012-07-07" : "2020-03-07";
+    request.setBundleEndDate(endDate);
     assertNotNull(request.getTmpDirectory());
     assertNotNull(request.getBundleDirectory());
     BundleBuildResponse response = new BundleBuildResponse(""
         + System.currentTimeMillis());
     assertEquals(0, response.getStatusList().size());
 
+
     // step 1
     _service.download(request, response);
     assertNotNull(response.getGtfsList());
-    assertEquals(1, response.getGtfsList().size());
-
+    int expected = (mode == 1) ? 1 : 6;
+    assertEquals(expected, response.getGtfsList().size());
+    expected = (mode == 1) ? 1 : 6;
     assertNotNull(response.getStifZipList());
-    assertEquals(1, response.getStifZipList().size());
+    assertEquals(expected, response.getStifZipList().size());
      
     assertNotNull(response.getStatusList());
     assertTrue(response.getStatusList().size() > 0);
