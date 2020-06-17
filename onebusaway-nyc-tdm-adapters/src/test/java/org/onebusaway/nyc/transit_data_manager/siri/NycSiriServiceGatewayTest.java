6e8b62e0292ef4ed27d2cd6a4dd1e92049a9dec6
sheldonabrown <sheldonb@gmail.com>
2020-06-17 08:39:28 -0400

    unit test fix

--- onebusaway-nyc-tdm-adapters/src/test/java/org/onebusaway/nyc/transit_data_manager/siri/NycSiriServiceGatewayTest.java
+++ onebusaway-nyc-tdm-adapters/src/test/java/org/onebusaway/nyc/transit_data_manager/siri/NycSiriServiceGatewayTest.java
@@ -32,6 +32,7 @@ public class NycSiriServiceGatewayTest extends NycSiriServiceGateway {
   @Test
   public void testGetPtSituationAsServiceAlertBean() {
     SiriHelper siriHelper = new SiriHelper();
+    // THE BULLET IS INTENTIONAL!!!
     PtSituationElementStructure ptSituation = siriHelper.createPtSituationElementStructure(
         "summaryText", "• descriptionText    ", "    MTA NYCT_123",
         "2011-11-08T00:00:00.000Z", "", "MTA NYCT_B63", "statusType");
@@ -41,7 +42,8 @@ public class NycSiriServiceGatewayTest extends NycSiriServiceGateway {
     assertNotNull(serviceAlertBean.getSummaries());
     assertEquals("summaryText",
         serviceAlertBean.getSummaries().get(0).getValue());
-    assertEquals(" descriptionText",
+    // swap bullet for escaped xml
+    assertEquals("&#x2022; descriptionText",
         serviceAlertBean.getDescriptions().get(0).getValue());
     assertEquals("MTA NYCT_123", serviceAlertBean.getId());
     assertEquals(
