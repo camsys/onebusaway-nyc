1020b7cd4d6d16a6c0777e99ee2e7f02230b8458
carabalb <lennycaraballo@gmail.com>
2020-09-09 01:42:50 -0400

    Added TDM proxying of APC data

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/nyc/ApcIntegrationServiceImpl.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/nyc/ApcIntegrationServiceImpl.java
@@ -27,11 +27,50 @@ import java.util.Date;
 
 public class ApcIntegrationServiceImpl extends ApcQueueListenerTask {
 
+    // how recent the occupancy record needs to be for usage
+    public static final int MAX_AGE_MILLIS = 6 * 60 * 1000; // 6 minutes
+
+    // in progress read connection timeout in millis
+    private static final int TIMEOUT_CONNECTION = 5000;
+    // socket connection timeout in millis
+    private static final int TIMEOUT_SOCKET = 5000;
+
     @Autowired
     private NycTransitDataService _tds;
 
     @Autowired
     private VehicleOccupancyListener _listener;
+
+    private Map<AgencyAndId, VehicleOccupancyRecord> _vehicleToQueueOccupancyCache = new ConcurrentHashMap<AgencyAndId, VehicleOccupancyRecord>();
+
+    /**
+     * Poll webservice at getRawCountUrl() for raw counts.  Alternatively
+     * pull APC levels off queue if false.
+     */
+    private boolean getRawCounts() {
+        return getRawCountUrl() != null;
+    }
+
+
+    public String getRawCountUrl() {
+        return _configurationService.getConfigurationValueAsString("tds.apcRawCountUrl", null);
+    }
+
+    @Autowired
+    private ThreadPoolTaskScheduler _taskScheduler;
+
+    @PostConstruct
+    public void setup() {
+        super.setup();
+        if (!getRawCounts()) {
+            _log.error("url not configured, Raw Count polling disabled.");
+            return;
+        }
+
+
+        RawCountPollerThread thread = new RawCountPollerThread(_tds, _listener, _vehicleToQueueOccupancyCache, getRawCountUrl());
+        _taskScheduler.scheduleWithFixedDelay(thread, 30 * 1000);
+    }
 
     @Override
     protected void processResult(NycVehicleLoadBean message, String contents) {
@@ -48,5 +87,111 @@ public class ApcIntegrationServiceImpl extends ApcQueueListenerTask {
         vor.setRouteId(message.getRoute());
         vor.setDirectionId(message.getDirection());
         return vor;
+    }
+
+    public static class RawCountPollerThread extends Thread {
+        private NycTransitDataService tds;
+        private VehicleOccupancyListener listener;
+        private Map<AgencyAndId, VehicleOccupancyRecord> vehicleToQueueOccupancyCache;
+        private String url;
+
+        public RawCountPollerThread(NycTransitDataService tds,
+                                    VehicleOccupancyListener listener,
+                                    Map<AgencyAndId, VehicleOccupancyRecord> vehicleToQueueOccupancyCache,
+                                    String url) {
+            this.tds = tds;
+            this.listener = listener;
+            this.vehicleToQueueOccupancyCache = vehicleToQueueOccupancyCache;
+            this.url = url;
+        }
+
+
+        public void run() {
+            _log.info("retrieving feed at " + url);
+            int count = 0;
+            Map<AgencyAndId, ApcLoadData> recordMap = getFeed();
+
+            // here we assume the web service feed will be the superset of data
+            for (AgencyAndId vehicleId : recordMap.keySet()) {
+                count++;
+                VehicleOccupancyRecord vor = merge(vehicleId, vehicleToQueueOccupancyCache, recordMap.get(vehicleId));
+                listener.handleVehicleOccupancyRecord(vor);
+            }
+            _log.info("retrieve complete with " + count + " entries");
+        }
+
+        // package private for unit tests
+         Map<AgencyAndId, ApcLoadData> getFeed() {
+            Map<AgencyAndId, ApcLoadData> results = new HashMap<AgencyAndId, ApcLoadData>();
+            HttpGet get = new HttpGet(url);
+            try {
+                HttpResponse response = getHttpClient().execute(get);
+                InputStreamReader reader = new InputStreamReader(response.getEntity().getContent());
+                Gson gson = new Gson();
+                // we need to give a hint on how to load a map
+                Type apcMapType = new TypeToken<Map<String, ApcLoadData>>() {}.getType();
+                Map<String, ApcLoadData> map = gson.fromJson(reader, apcMapType);
+                // for our results we want fully qualified agency id to be the key
+                for (String key : map.keySet()) {
+                    ApcLoadData ald = map.get(key);
+                    if (ald.getVehicleAsId() != null)
+                        results.put(ald.getVehicleAsId(), ald);
+                }
+                return results;
+
+            } catch (IOException ioe) {
+                _log.error("failure retrieving " + url + ", " + ioe, ioe);
+            }
+            return results;
+        }
+
+        private  VehicleOccupancyRecord merge(AgencyAndId vehicleId,
+                                              Map<AgencyAndId, VehicleOccupancyRecord> cache,
+                                              ApcLoadData additionalApcData) {
+            VehicleOccupancyRecord queueRecord = cache.get(vehicleId);
+
+            // CASE 1:  no ApcData element from webservice, return whatever matches from cache
+            if (additionalApcData == null  || !isTimely(additionalApcData.getTimestamp())) {
+                return queueRecord;
+            }
+            // CASE 2:  no enumeration from queue, return ApcData as VehicleOccupancyRecord
+            if (queueRecord == null || !isTimely(queueRecord.getTimestamp().getTime())) {
+                return toVehicleOccupancyRecord(additionalApcData);
+            }
+            // CASE 3:  we have both, merge raw counts/capacity into queue record and return
+            VehicleOccupancyRecord vor = queueRecord.deepCopy();
+            vor.setRawCount(additionalApcData.getEstLoadAsInt());
+            vor.setCapacity(additionalApcData.getEstCapacityAsInt());
+            vor.setVehicleId(vehicleId);
+            vor.setTimestamp(new Date(additionalApcData.getTimestamp()));
+            return vor;
+
+        }
+
+        private boolean isTimely(long time) {
+            long delta = Math.abs(System.currentTimeMillis() - time);
+            return delta < MAX_AGE_MILLIS;
+        }
+
+        private VehicleOccupancyRecord toVehicleOccupancyRecord(ApcLoadData data) {
+            VehicleOccupancyRecord vor = new VehicleOccupancyRecord();
+            vor.setVehicleId(data.getVehicleAsId());
+            vor.setTimestamp(new Date(data.getTimestamp()));
+            vor.setCapacity(data.getEstCapacityAsInt());
+            vor.setRawCount(data.getEstLoadAsInt());
+            return vor;
+        }
+
+        // allow unit tests to override
+        protected HttpClient getHttpClient() {
+            HttpParams httpParams = new BasicHttpParams();
+            // recover from bad network conditions
+            HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_CONNECTION);
+            HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_SOCKET);
+            HttpClient httpClient = new DefaultHttpClient(httpParams);
+
+            return httpClient;
+        }
+
     }
 }
---
+++ onebusaway-nyc-transit-data-federation/src/test/java/org/onebusaway/nyc/transit_data_federation/impl/nyc/ApcIntegrationServiceImplTest.java
@@ -0,0 +1,67 @@
+package org.onebusaway.nyc.transit_data_federation.impl.nyc;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.realtime.api.VehicleOccupancyRecord;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+
+public class ApcIntegrationServiceImplTest {
+
+    String apcJson = "{\"9507\":{\"vehicle\":9507,\"timestamp\":1592243923000,\"agencyId\":\"MTA NYCT\",\"estCapacity\":null,\"estLoad\":\"6\",\"loadDescription\":\"6\",\"vehicleDescription\":\"2018 NEW FLYER HYBRID  XDE40\"},\"9509\":{\"vehicle\":9509,\"timestamp\":1592243917000,\"agencyId\":\"MTA NYCT\",\"estCapacity\":null,\"estLoad\":\"17\",\"loadDescription\":\"17\",\"vehicleDescription\":\"2018 NEW FLYER HYBRID  XDE40\"}}";
+
+    @Test
+    public void testGetFeed() throws Exception {
+        //feed is object map key:vehicleId, value: ApcData
+        String url = "http://example.com/feed"; // we mock out the results, this isn't used
+        Map<AgencyAndId, VehicleOccupancyRecord> map = new HashMap<AgencyAndId, VehicleOccupancyRecord>();
+        ApcIntegrationServiceImpl impl = new ApcIntegrationServiceImpl();
+        final HttpClient httpClient = Mockito.mock(HttpClient.class);
+        HttpResponse response = Mockito.mock(HttpResponse.class);
+        HttpEntity entity = Mockito.mock(HttpEntity.class);
+        Mockito.when(entity.getContent()).thenReturn(getAsStream(apcJson));
+        Mockito.when(response.getEntity()).thenReturn(entity);
+        Mockito.when(httpClient.execute((HttpUriRequest) any())).thenReturn(response);
+        ApcIntegrationServiceImpl.RawCountPollerThread thread
+                = new ApcIntegrationServiceImpl.RawCountPollerThread(null, null,
+                map,
+                url) {
+            @Override
+            protected HttpClient getHttpClient() {
+                return httpClient;
+            }
+        };
+        Map<AgencyAndId, ApcLoadData> feed = thread.getFeed();
+        assertNotNull(feed);
+        assertEquals(2, feed.size());
+        assertNotNull(feed.get(new AgencyAndId("MTA NYCT","9507")));
+        assertNotNull(feed.get(new AgencyAndId("MTA NYCT","9509")));
+
+        ApcLoadData a9507 = feed.get(new AgencyAndId("MTA NYCT","9507"));
+        assertEquals("9507", a9507.getVehicle());
+        assertEquals("MTA NYCT_9507", a9507.getVehicleAsId().toString());
+        assertEquals(1592243923000l, a9507.getTimestamp());
+        assertEquals(null, a9507.getEstCapacityAsInt());
+        assertEquals(new Integer(6), a9507.getEstLoadAsInt());
+        assertEquals("6", a9507.getLoadDescription());
+        assertNull(a9507.getEstCapacityAsInt());
+
+    }
+
+    private InputStream getAsStream(String s) {
+        return new ByteArrayInputStream(s.getBytes());
+    }
+}
