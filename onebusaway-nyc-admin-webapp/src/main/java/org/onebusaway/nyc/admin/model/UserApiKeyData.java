5395b649cc30cb3549f3e31bde40a4cd66e26056
carabalb <lennycaraballo@gmail.com>
2020-10-27 12:46:59 -0400

    [OBANYC-2992] Add User API Key Registration Viewer to Admin Page

---
+++ onebusaway-nyc-admin-webapp/src/main/java/org/onebusaway/nyc/admin/model/UserApiKeyData.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C)  2011 Metropolitan Transportation Authority
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onebusaway.nyc.admin.model;
+import org.hibernate.annotations.Index;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "obanyc_api_key_data")
+public class UserApiKeyData {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="created")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Column(name="email", nullable = false, length=512)
+    private String email;
+
+    @Column(name="project_name")
+    private String projectName;
+
+    @Column(name="project_url", length=512)
+    private String projectUrl;
+
+    @Column(name="platform")
+    private String platform;
+
+    @Column(name="api_key")
+    @Index(name = "api_key_index")
+    private String apiKey;
+
+    @Column(name="comment", length=1024)
+    private String comment;
+
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectUrl() {
+        return projectUrl;
+    }
+
+    public void setProjectUrl(String projectUrl) {
+        this.projectUrl = projectUrl;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+}
---
+++ onebusaway-nyc-admin-webapp/src/test/java/org/onebusaway/nyc/admin/service/impl/UserApiKeyDataServiceImplTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C)  2011 Metropolitan Transportation Authority
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onebusaway.nyc.admin.service.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.onebusaway.nyc.admin.model.UserApiKeyData;
+import org.onebusaway.nyc.admin.service.UserApiKeyDataService;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserApiKeyDataServiceImplTest {
+
+    private SessionFactory _sessionFactory;
+    private UserApiKeyDataService _service;
+
+    @Before
+    public void setup() throws IOException {
+
+        Configuration config = new Configuration();
+        config = config.configure("org/onebusaway/nyc/admin/hibernate-configuration.xml");
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                config.getProperties()). buildServiceRegistry();
+        _sessionFactory = config.buildSessionFactory(serviceRegistry);
+
+        _service = new UserApiKeyDataServiceImpl();
+        _service.setSesssionFactory(_sessionFactory);
+    }
+
+    @After
+    public void teardown() {
+        if (_sessionFactory != null)
+            _sessionFactory.close();
+    }
+
+    private Session getSession(){
+        return _sessionFactory.getCurrentSession();
+    }
+
+    @Test
+    public void getAllUserApiKeyDataTest(){
+        Transaction t = getSession().beginTransaction();
+        UserApiKeyData data = new UserApiKeyData();
+        data.setName("test user");
+        data.setEmail("testemail@email.com");
+        data.setProjectName("test project name");
+        data.setProjectUrl("http://bustime.mta.info");
+        data.setPlatform("web, andriod, ios, amazon echo, other");
+        data.setApiKey("API-KEY-HERE");
+        _service.saveOrUpdateApiKeyData(data);
+
+        List<UserApiKeyData> allData = _service.getAllUserApiKeyData();
+        assertEquals(1, allData.size());
+
+        UserApiKeyData dbData =  allData.get(0);
+        assertEquals(new Long(1), data.getId());
+        assertEquals(data.getName(), dbData.getName());
+        assertEquals(data.getEmail(), dbData.getEmail());
+        assertEquals(data.getProjectName(), dbData.getProjectName());
+        assertEquals(data.getProjectUrl(), dbData.getProjectUrl());
+        assertEquals(data.getPlatform(), dbData.getPlatform());
+        assertEquals(data.getApiKey(), dbData.getApiKey());
+        t.commit();
+
+    }
+
+    @Test
+    public void getAllUserApiKeyDataPagingTest(){
+        Transaction t = getSession().beginTransaction();
+        for(int i=1; i < 16; i++){
+            UserApiKeyData data = new UserApiKeyData();
+            data.setName(String.format("test user %s", i));
+            data.setEmail(String.format("testemail%s@email.com", i));
+            data.setProjectName(String.format("test project name %s",1));
+            data.setProjectUrl("http://bustime.mta.info");
+            data.setPlatform("web, andriod, ios, amazon echo, other");
+            data.setApiKey(String.format("API-KEY-HERE-%s", 1));
+            _service.saveOrUpdateApiKeyData(data);
+        }
+
+        List<UserApiKeyData> allData = _service.getAllUserApiKeyData();
+        assertEquals(15, allData.size());
+
+        // test max results
+        List<UserApiKeyData> pagedData = _service.getAllUserApiKeyDataPaged(0, 10);
+        assertEquals(10, pagedData.size());
+        assertEquals("test user 1", pagedData.get(0).getName());
+        assertEquals("test user 10", pagedData.get(9).getName());
+
+        // test first result offset by 1
+        pagedData = _service.getAllUserApiKeyDataPaged(1, 10);
+        assertEquals("test user 2", pagedData.get(0).getName());
+        assertEquals("test user 11", pagedData.get(9).getName());
+
+        // test first result null
+        pagedData = _service.getAllUserApiKeyDataPaged(null, 5);
+        assertEquals(5, pagedData.size());
+        assertEquals("test user 1", pagedData.get(0).getName());
+
+        // test first result and max results null
+        pagedData = _service.getAllUserApiKeyDataPaged(null, null);
+        assertEquals(15, pagedData.size());
+
+        // test offset greater than size
+        pagedData = _service.getAllUserApiKeyDataPaged(16, 10);
+        assertEquals(0, pagedData.size());
+
+        // test maxResults greater than size
+        pagedData = _service.getAllUserApiKeyDataPaged(0, 16);
+        assertEquals(15, pagedData.size());
+
+        t.commit();
+    }
+
+
+}
