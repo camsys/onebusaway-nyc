9bd454cd5c7777481bfd6e2f2899295cd7084430
carabalb <lennycaraballo@gmail.com>
2020-11-03 13:59:04 -0500

    Updating integration-test webapp data-source files
    - Adding dummy UnassignedVehicleService bean to vtw data-source
    - Update data-source beans
    -  Removing dependency clashes

--- onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/services/unassigned/UnassignedVehicleService.java
+++ onebusaway-nyc-vehicle-tracking/src/main/java/org/onebusaway/nyc/vehicle_tracking/services/unassigned/UnassignedVehicleService.java
@@ -23,8 +23,6 @@ import java.net.URL;
 public interface UnassignedVehicleService {
 
     void setUrl(URL url);
-    void setup();
-    void destroy();
     // for testing only!
     void enqueueTestRecord(RealtimeEnvelope envelope);
 }
