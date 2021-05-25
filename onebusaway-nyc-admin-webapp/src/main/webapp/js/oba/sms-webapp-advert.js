94b018a9e6ce296812edf4b4b8372838126111d7
caylasavitzky <caysavitzky@gmail.com>
2021-05-25 19:53:11 -0400

    refactor to use ajax and rely on pre-existing class

--- onebusaway-nyc-admin-webapp/src/main/webapp/js/oba/sms-webapp-advert.js
+++ onebusaway-nyc-admin-webapp/src/main/webapp/js/oba/sms-webapp-advert.js
@@ -21,7 +21,14 @@
     Time: 11:56 PM
 */
 
+var csrfParameter = "";
+var csrfToken = "";
+var csrfHeader = "";
+
 $(function() {
+    csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
+    csrfHeader = $("meta[name='_csrf_header']").attr("content");
+    csrfToken = $("meta[name='_csrf_token']").attr("content");
     $('.advert_conditional_display').hide();
     $('#showAd').change(function(){
         if($('#showAd').val() == 'true') {
@@ -31,3 +38,78 @@ $(function() {
         }
     });
 });
+
+
+function saveParameters() {
+    var data = buildData();
+    data = {"params": data}
+
+    if(data != null) {
+        $.ajax({
+            url: "parameters!saveParameters.action",
+            type: "POST",
+            dataType: "json",
+            data: data,
+            traditional: true,
+            success: function(response) {
+                if(response.saveSuccess) {
+                    $("#result #message").text("Your changes have been saved. " +getTime());
+                    $("#results #message").delay(10000).fadeOut(5000);
+                } else {
+                    alert("Failed to save parameters. Please try again.");
+                    $("#result #message").text("Your changes were not saved. " +getTime());
+                }
+            },
+            error: function(request) {
+                alert("Error saving parameter values");
+            }
+        });
+    } else {
+        alert("Cannot save parameters. One or more values is blank");
+    }
+}
+
+
+function buildData() {
+    var data = new Array();
+    var invalid = false;
+    var elements = $(".ad_update");
+    for(var i=0; i<elements.length; i++) {
+        var value = elements[i].value;
+        if(value!=""&value!=null) {
+            var key = elements[i].name;
+            data.push(key + ":" + value);
+        }
+    }
+    if(invalid) {
+        data = null;
+    }
+    return data;
+}
+
+function getTime() {
+    var lastUpdateTime = new Date();
+    var hours = lastUpdateTime.getHours();
+    var meridian;
+    if(hours > 12) {
+        hours = hours - 12;
+        meridian = "PM";
+    } else {
+        if(hours == 12) {
+            meridian = "PM";
+        } else {
+            meridian = "AM";
+        }
+    }
+    var minutes = lastUpdateTime.getMinutes();
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    var seconds = lastUpdateTime.getSeconds();
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ":" +  minutes + ":" + seconds + " " +meridian;
+
+    return time;
+}
