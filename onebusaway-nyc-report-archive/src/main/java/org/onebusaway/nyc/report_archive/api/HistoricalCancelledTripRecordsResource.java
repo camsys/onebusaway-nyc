b3f9ce657f2e2026a2e07d51af602ffc43df8421
carabalb <lennycaraballo@gmail.com>
2022-02-10 15:55:19 -0500

    A bit of clean up and adding license headers

--- onebusaway-nyc-report-archive/src/main/java/org/onebusaway/nyc/report_archive/api/HistoricalCancelledTripRecordsResource.java
+++ onebusaway-nyc-report-archive/src/main/java/org/onebusaway/nyc/report_archive/api/HistoricalCancelledTripRecordsResource.java
@@ -1,16 +1,28 @@
+/**
+ * Copyright (C) 2022 Metropolitan Transportation Authority
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.onebusaway.nyc.report_archive.api;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-import org.hibernate.query.Query;
-import org.onebusaway.nyc.report_archive.api.HistoricalRecordsResource;
 import org.onebusaway.nyc.report_archive.api.json.HistoricalCancelledTripRecordsMessage;
 import org.onebusaway.nyc.report_archive.impl.CancelledTripDaoImpl;
 import org.onebusaway.nyc.report_archive.model.NycCancelledTripRecord;
-import org.onebusaway.nyc.report_archive.result.HistoricalRecord;
 import org.onebusaway.nyc.report_archive.services.CancelledTripDao;
-import org.onebusaway.nyc.report_archive.services.HistoricalRecordsDao;
 import org.onebusaway.nyc.report.api.json.JsonTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,9 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.io.StringWriter;
