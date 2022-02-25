efa170e06b50b06497742bae6dd17c6484e39229
carabalb <lennycaraballo@gmail.com>
2022-02-25 10:24:36 -0500

    OBANYC-3140 - Fix for new alerts info

--- onebusaway-nyc-gtfsrt-lib/src/main/java/org/onebusaway/nyc/transit_data_manager/util/NycSiriUtil.java
+++ onebusaway-nyc-gtfsrt-lib/src/main/java/org/onebusaway/nyc/transit_data_manager/util/NycSiriUtil.java
@@ -177,7 +177,14 @@ public class NycSiriUtil {
     // NEW!  advice has longer versions of description
     ServiceAlerts.TranslatedString description = translation(ptSituation.getAdvice());
     if (description != null) {
-      serviceAlert.setDescriptions(naturalLanguageStringBeanFromTranslatedString(description, true));
+      // Handle new alerts advice field
+      // Only append advice to description is description is available
+      DefaultedTextStructure advice = ptSituation.getAdvice();
+      if(advice != null){
+        description.setValue(description.getValue() + System.lineSeparator() + advice.getValue());
+      }
+      ServiceAlerts.TranslatedString translatedDescription = translation(description);
+      serviceAlert.setDescriptions(naturalLanguageStringBeanFromTranslatedString(translatedDescription, true));
     }
   }
 
