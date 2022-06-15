2e0880d9202125f8e16d5f8645ff6bfcdd8d241e
Jared Koester <jkoester@camsys.com>
2022-06-15 14:14:22 -0400

    Fix a class and point pom to new app-mods version

--- onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/service_alerts/NycServiceAlertsRecordServiceImpl.java
+++ onebusaway-nyc-transit-data-federation/src/main/java/org/onebusaway/nyc/transit_data_federation/impl/service_alerts/NycServiceAlertsRecordServiceImpl.java
@@ -16,19 +16,20 @@
 
 package org.onebusaway.nyc.transit_data_federation.impl.service_alerts;
 
+import org.onebusaway.alerts.impl.ServiceAlertRecord;
+import org.onebusaway.alerts.service.ServiceAlertsService;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.transit_data.model.service_alerts.SituationQueryBean;
-import org.onebusaway.transit_data_federation.impl.service_alerts.ServiceAlertRecord;
+
 import org.onebusaway.transit_data_federation.services.blocks.BlockInstance;
 import org.onebusaway.transit_data_federation.services.blocks.BlockTripInstance;
-import org.onebusaway.transit_data_federation.services.service_alerts.ServiceAlertsRecordService;
 import org.onebusaway.transit_data_federation.services.transit_graph.BlockStopTimeEntry;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class NycServiceAlertsRecordServiceImpl implements ServiceAlertsRecordService {
+public class NycServiceAlertsRecordServiceImpl implements ServiceAlertsService {
 
     @Override
     public void loadServiceAlerts() {
@@ -86,17 +87,32 @@ public class NycServiceAlertsRecordServiceImpl implements ServiceAlertsRecordSer
     }
 
     @Override
-    public List<ServiceAlertRecord> getServiceAlertsForStopCall(long l, BlockInstance blockInstance, BlockStopTimeEntry blockStopTimeEntry, AgencyAndId agencyAndId) {
+    public List<ServiceAlertRecord> getServiceAlertsForRouteId(long l, AgencyAndId agencyAndId) {
         return null;
     }
 
     @Override
-    public List<ServiceAlertRecord> getServiceAlertsForVehicleJourney(long l, BlockTripInstance blockTripInstance, AgencyAndId agencyAndId) {
+    public List<ServiceAlertRecord> getServiceAlertsForRouteAndStopId(long l, AgencyAndId agencyAndId, AgencyAndId agencyAndId1) {
+        return null;
+    }
+
+    @Override
+    public List<ServiceAlertRecord> getServiceAlertsForTripAndStopId(long l, AgencyAndId agencyAndId, AgencyAndId agencyAndId1) {
+        return null;
+    }
+
+    @Override
+    public List<ServiceAlertRecord> getServiceAlertsForRouteAndDirection(long l, AgencyAndId agencyAndId, AgencyAndId agencyAndId1, String s) {
         return null;
     }
 
     @Override
     public List<ServiceAlertRecord> getServiceAlerts(SituationQueryBean situationQueryBean) {
+        return null;
+    }
+
+    @Override
+    public List<ServiceAlertRecord> createOrUpdateServiceAlerts(String s, List<ServiceAlertRecord> list) {
         return null;
     }
 }
