58be531b99a34792ef880adbc0d6520bb4e95b9b
caylasavitzky <caysavitzky@gmail.com>
2023-11-13 11:39:30 -0500

    setting default integer values to -1

--- onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/providers/CustomParamConverterProvider.java
+++ onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/providers/CustomParamConverterProvider.java
@@ -1,30 +1,47 @@
 package org.onebusaway.api.providers;
 
-import org.eclipse.emf.ecore.util.EcoreValidator;
-import org.glassfish.hk2.utilities.reflection.ParameterizedTypeImpl;
 import org.joda.time.DateTime;
 import org.onebusaway.nyc.presentation.impl.conversion.NycDateConverter;
 import org.onebusaway.nyc.presentation.impl.conversion.NycDateTimeConverter;
 
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.ParamConverterProvider;
 import javax.ws.rs.ext.Provider;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
-//@Provider
+@Provider
 public class CustomParamConverterProvider implements ParamConverterProvider {
 
 
     @Override
     public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+        ParamConverter<T> converter = null;
         if (rawType.equals(DateTime.class))
-            return (ParamConverter<T>) new NycDateTimeConverter();
+            converter =  (ParamConverter<T>) new NycDateTimeConverter();
         if (rawType.equals(Date.class))
-            return (ParamConverter<T>) new NycDateConverter();
-        return null;
+            converter = (ParamConverter<T>) new NycDateConverter();
+        if(rawType.equals(Integer.TYPE)) {
+            return (ParamConverter<T>) new IntegerConverter();
+        }
+
+        return converter;
+    }
+
+
+    class IntegerConverter implements ParamConverter<Integer> {
+        @Override
+        public Integer fromString(String value) {
+            if(value==null || value.equals("")){
+                return -1;
+            }
+            return Integer.valueOf(value);
+        }
+
+        @Override
+        public String toString(Integer value) {
+            return String.valueOf(value);
+        }
     }
 }
