66bcafa2d3370e797d026a993ea3b8f069732671
carabalb <lennycaraballo@gmail.com>
2024-01-04 16:01:27 -0500

    One more fix

--- onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/TripUpdateServiceImpl.java
+++ onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/TripUpdateServiceImpl.java
@@ -116,7 +116,7 @@ public class TripUpdateServiceImpl extends AbstractFeedMessageService {
                 } else {
                     List<TimepointPredictionRecord> tprs = _transitDataService.getPredictionRecordsForVehicleAndTrip(vehicle.getVehicleId(), trip.getId());
                     boolean isSpooking = vehicle != null && vehicle.getPhase() != null && !vehicle.getPhase().equalsIgnoreCase(EVehiclePhase.SPOOKING.toLabel());
-                    if ((tprs == null || tprs.isEmpty()) && isSpooking){
+                    if ((tprs == null || tprs.isEmpty()) && !isSpooking){
                         _log.debug("no tprs for time=" + new Date(time));
                         break;
                     }
