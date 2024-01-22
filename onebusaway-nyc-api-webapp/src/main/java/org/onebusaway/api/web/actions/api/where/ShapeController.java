9624e7e6a9fd8086eb405419f3081b5f58c694c3
caylasavitzky <caysavitzky@gmail.com>
2024-01-22 14:12:08 -0500

    update ShapeController (was not working)

--- onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/web/actions/api/where/ShapeController.java
+++ onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/web/actions/api/where/ShapeController.java
@@ -22,13 +22,15 @@ import org.onebusaway.transit_data.services.TransitDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.onebusaway.api.model.ResponseBean;
 
-@Path("/where/shape/{shapeId}")
-public class ShapeResponse extends ApiActionSupport {
+@RestController
+@RequestMapping("/where/shape/{shapeId}")
+public class ShapeController extends ApiActionSupport {
 
   private static final long serialVersionUID = 1L;
 
@@ -37,33 +39,25 @@ public class ShapeResponse extends ApiActionSupport {
   @Autowired
   private TransitDataService _service;
 
-  private String _id;
 
-  public ShapeResponse() {
+
+  public ShapeController() {
     super(V2);
   }
 
-  @PathParam("shapeId")
-  public void setId(String id) {
-    _id = id;
-  }
 
-  public String getId() {
-    return _id;
-  }
-
-  @GET
-  public Response show() {
+  @GetMapping
+  public ResponseBean show(@PathVariable("shapeId") String id) {
 
     if (hasErrors())
-      return getValidationErrorsResponse();
+      return getValidationErrorsResponseBean();
 
-    EncodedPolylineBean shape = _service.getShapeForId(_id);
+    EncodedPolylineBean shape = _service.getShapeForId(id);
 
     if (shape == null)
-      return getResourceNotFoundResponse();
+      return getResourceNotFoundResponseBean();
 
     BeanFactoryV2 factory = getBeanFactoryV2();
-    return getOkResponse(factory.getResponse(shape));
+    return getOkResponseBean(factory.getResponse(shape));
   }
 }
