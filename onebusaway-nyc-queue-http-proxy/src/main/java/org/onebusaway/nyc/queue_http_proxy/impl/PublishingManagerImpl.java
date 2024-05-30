21edd4270a8d1bb2b9f1a5fca14408115b1014c2
lcaraballo <lennycaraballo@gmail.com>
2024-05-30 13:59:47 -0400

    High frequency AVL - send low freq avl to high freq queue as well

--- onebusaway-nyc-queue-http-proxy/src/main/java/org/onebusaway/nyc/queue_http_proxy/impl/PublishingManagerImpl.java
+++ onebusaway-nyc-queue-http-proxy/src/main/java/org/onebusaway/nyc/queue_http_proxy/impl/PublishingManagerImpl.java
@@ -98,6 +98,7 @@ public class PublishingManagerImpl implements PublishingManager{
             processMessage(vehicleId, vehicleTimestamp, message.toString());
         } else {
             publisher.send(message.toString());
+            highFreqPublisher.send(message.toString());
         }
     }
 
