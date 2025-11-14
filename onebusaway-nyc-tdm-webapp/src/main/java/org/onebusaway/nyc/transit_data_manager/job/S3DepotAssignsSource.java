c92611e2d3856aed2a3b4e87254babfbbb291207
Lenny Caraballo <lennycaraballo@gmail.com>
2025-11-14 01:16:10 -0500

    Fixes to S3DepotAssignsSource

--- onebusaway-nyc-tdm-webapp/src/main/java/org/onebusaway/nyc/transit_data_manager/job/S3DepotAssignsSource.java
+++ onebusaway-nyc-tdm-webapp/src/main/java/org/onebusaway/nyc/transit_data_manager/job/S3DepotAssignsSource.java
@@ -19,11 +19,10 @@ package org.onebusaway.nyc.transit_data_manager.job;
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 
@@ -33,27 +32,114 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
 
 /**
  * S3 implementation for fetching depot assignments
  */
 @Component
-public class S3DepotAssignsSource implements DepotAssignsSource {
+public class S3DepotAssignsSource implements DepotAssignsSource, ServletContextAware {
 
     private static final Logger _log = LoggerFactory.getLogger(S3DepotAssignsSource.class);
 
     private ConfigurationService _configurationService;
     private AmazonS3 _s3Client;
 
+    private String _username;
+    private String _password;
+    private String _bucketName;
+    private String _bucketKey;
+
+    private AWSCredentials _awsCredentials;
+
+
     @Autowired
     public void setConfigurationService(ConfigurationService configurationService) {
         _configurationService = configurationService;
     }
 
+    @PostConstruct
+    public void setup() {
+        try {
+            _log.info("setting up S3DepotAssignsSource with username=" + getUser()
+                    + " and bucket=" + getBucketName() + " and bucketKey=" + getBucketKey());
+            _awsCredentials = new BasicAWSCredentials(getUser(), getPassword());
+            _s3Client = new AmazonS3Client(_awsCredentials);
+        } catch (Exception ioe) {
+            _log.error("S3DepotAssignsSource setup failed, likely due to missing or invalid s3 credentials");
+            _log.error(ioe.toString());
+        }
+
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        if (servletContext != null) {
+            String keyId = servletContext.getInitParameter("s3.depotAssigns.user");
+            _log.info("servlet context provided s3.depotAssigns.user=" + keyId);
+            if (keyId != null) {
+                setKeyId(keyId);
+            }
+            String secretKey = servletContext.getInitParameter("s3.depotAssigns.password");
+            if (secretKey != null) {
+                setPassword(secretKey);
+            }
+            String bucketName = servletContext.getInitParameter("s3.depotAssigns.bucketName");
+            if (bucketName != null) {
+                _log.info("servlet context provided s3.depotAssigns.bucketName=" + bucketName);
+                setBucketName(bucketName);
+            } else {
+                _log.info("servlet context missing bucketName, using " + getBucketName());
+            }
+            String bucketKey = servletContext.getInitParameter("s3.depotAssigns.bucketKey");
+            if (bucketKey != null) {
+                _log.info("servlet context provided s3.depotAssigns.bucketKey=" + bucketKey);
+                setBucketKeyName(bucketKey);
+            } else {
+                _log.info("servlet context missing bucketKeyName, using " + getBucketKey());
+            }
+        }
+    }
+
+    private String getBucketKey() {
+        return _bucketKey;
+    }
+
+    private String getBucketName() {
+        return _bucketName;
+    }
+
+    private String getUser() {
+        return _username;
+    }
+
+    private String getPassword() {
+        return _password;
+    }
+
+    private void setBucketName(String bucketName) {
+        _bucketName = bucketName;
+    }
+
+    private void setBucketKeyName(String bucketKeyName) {
+        _bucketKey = bucketKeyName;
+    }
+
+    private void setPassword(String password) {
+        _password = password;
+    }
+
+    private void setKeyId(String keyId) {
+        _username = keyId;
+    }
+
     @Override
     public InputStream fetchDepotAssignments() throws IOException {
-        String bucket = getBucket();
-        String key = getKey();
+        String bucket = getBucketName();
+        String key = getBucketKey();
 
         if (StringUtils.isBlank(bucket) || StringUtils.isBlank(key)) {
             throw new IOException("S3 bucket or key not configured");
@@ -76,6 +162,10 @@ public class S3DepotAssignsSource implements DepotAssignsSource {
         }
     }
 
+    private AmazonS3 getS3Client() {
+        return _s3Client;
+    }
+
     @Override
     public String getSourceType() {
         return "S3";
@@ -83,8 +173,8 @@ public class S3DepotAssignsSource implements DepotAssignsSource {
 
     @Override
     public boolean isAvailable() {
-        String bucket = getBucket();
-        String key = getKey();
+        String bucket = getBucketName();
+        String key = getBucketKey();
 
         if (StringUtils.isBlank(bucket) || StringUtils.isBlank(key)) {
             return false;
@@ -93,110 +183,11 @@ public class S3DepotAssignsSource implements DepotAssignsSource {
         try {
             AmazonS3 s3Client = getS3Client();
             // Check if the bucket exists and is accessible
-            return s3Client.doesBucketExistV2(bucket);
+            return s3Client.doesBucketExist(bucket);
         } catch (Exception e) {
             _log.warn("Unable to verify S3 availability", e);
             return false;
         }
     }
 
-    /**
-     * Get or create the S3 client
-     */
-    private synchronized AmazonS3 getS3Client() {
-        if (_s3Client == null) {
-            String region = getRegion();
-            String accessKeyId = getAccessKeyId();
-            String secretAccessKey = getSecretAccessKey();
-
-            AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
-
-            if (StringUtils.isNotBlank(region)) {
-                builder.withRegion(region);
-            }
-
-            // Use explicit credentials if provided, otherwise use default credential provider chain
-            if (StringUtils.isNotBlank(accessKeyId) && StringUtils.isNotBlank(secretAccessKey)) {
-                BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
-                builder.withCredentials(new AWSStaticCredentialsProvider(awsCredentials));
-            } else {
-                // This will use IAM role credentials if running on EC2, or environment variables, etc.
-                builder.withCredentials(DefaultAWSCredentialsProviderChain.getInstance());
-            }
-
-            _s3Client = builder.build();
-        }
-        return _s3Client;
-    }
-
-    private String getBucket() {
-        String bucket = System.getProperty("AWS_S3_DEPOT_ASSIGNS_BUCKET_NAME");
-
-        if (StringUtils.isNotBlank(bucket)) {
-            _log.debug("Using S3 bucket: {}", bucket);
-        }
-
-        return bucket;
-    }
-
-    private String getKey() {
-        String keyPattern = System.getProperty("AWS_S3_DEPOT_ASSIGNS_KEY");
-
-        if (StringUtils.isBlank(keyPattern)) {
-            keyPattern = _configurationService.getConfigurationValueAsString("tdm.depotAssigns.s3.key", null);
-        }
-
-        if (StringUtils.isBlank(keyPattern)) {
-            return null;
-        }
-
-        return keyPattern;
-    }
-
-    private String getRegion() {
-        String region = System.getProperty("AWS_REGION");
-
-        if (StringUtils.isBlank(region)) {
-            region = System.getProperty("AWS_DEFAULT_REGION");
-        }
-
-        if (StringUtils.isBlank(region)) {
-            region = _configurationService.getConfigurationValueAsString("tdm.depotAssigns.s3.region", "us-east-1");
-        }
-
-        return region;
-    }
-
-    private String getAccessKeyId() {
-        String accessKeyId = System.getProperty("AWS_ACCESS_KEY_ID");
-
-        if (StringUtils.isBlank(accessKeyId)) {
-            accessKeyId = System.getProperty("s3.user");
-        }
-
-        if (StringUtils.isBlank(accessKeyId)) {
-            accessKeyId = System.getProperty("s3.username");
-        }
-
-        if (StringUtils.isBlank(accessKeyId)) {
-            accessKeyId = _configurationService.getConfigurationValueAsString("tdm.depotAssigns.s3.accessKeyId", null);
-        }
-
-        return accessKeyId;
-    }
-
-
-    private String getSecretAccessKey() {
-        String secretAccessKey = System.getProperty("AWS_SECRET_ACCESS_KEY");
-
-        if (StringUtils.isBlank(secretAccessKey)) {
-            secretAccessKey = System.getProperty("s3.password");
-        }
-
-        if (StringUtils.isBlank(secretAccessKey)) {
-            secretAccessKey = _configurationService.getConfigurationValueAsString("tdm.depotAssigns.s3.secretAccessKey", null);
-        }
-
-        return secretAccessKey;
-    }
 }
