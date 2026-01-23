587bce590a83bf038de0235cfd47f95a086f7363
Lenny Caraballo <lennycaraballo@gmail.com>
2026-01-22 21:48:51 -0500

    Revert "Updating OBA API Situations"
    This reverts commit 643a76071ecb7c2dfa7891b566a3d53d7d0e3d4b.

--- onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/serializers/xml/CDataPolicy.java
+++
@@ -1,6 +0,0 @@
-package org.onebusaway.api.serializers.xml;
-
-@FunctionalInterface
-interface CDataPolicy {
-  boolean shouldWrap(XmlScope scope, String nodeName);
-}
--- onebusaway-nyc-api-webapp/src/main/java/org/onebusaway/api/serializers/xml/CDataSanitizer.java
+++
@@ -1,7 +0,0 @@
-package org.onebusaway.api.serializers.xml;
-
-final class CDataSanitizer {
-  static String sanitize(String text) {
-    return text == null ? "" : text.replace("]]>", "]]]]><![CDATA[>");
-  }
-}
