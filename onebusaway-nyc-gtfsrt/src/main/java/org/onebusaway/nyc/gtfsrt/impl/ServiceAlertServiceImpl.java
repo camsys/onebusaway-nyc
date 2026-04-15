7419caba86447bad4eb0d14c641d12d2b16d15a3
msalvatore <msalvatore@camsys.com>
2026-04-15 08:42:16 -0400

    [OBANYC-4085] threading to cache alerts

--- onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/ServiceAlertServiceImpl.java
+++ onebusaway-nyc-gtfsrt/src/main/java/org/onebusaway/nyc/gtfsrt/impl/ServiceAlertServiceImpl.java
@@ -16,26 +16,35 @@
 package org.onebusaway.nyc.gtfsrt.impl;
 
 import com.google.transit.realtime.GtfsRealtime.*;
+import org.onebusaway.container.refresh.Refreshable;
 import org.onebusaway.nyc.gtfsrt.service.ServiceAlertFeedBuilder;
 import org.onebusaway.nyc.transit_data.services.NycTransitDataService;
+import org.onebusaway.nyc.util.configuration.ConfigurationService;
 import org.onebusaway.transit_data.model.AgencyWithCoverageBean;
 import org.onebusaway.transit_data.model.ListBean;
 import org.onebusaway.transit_data.model.service_alerts.ServiceAlertBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Build the service alerts feed for the current time.
+ * Caches the feed via a background refresh thread; refresh interval is
+ * configurable through the ConfigurationService.
  */
 @Component
 public class ServiceAlertServiceImpl extends AbstractFeedMessageService {
 
+    private static final String CONFIG_ALERTS_REFRESH_INTERVAL = "gtfsrt.alertRefreshInterval";
+
     private ServiceAlertFeedBuilder _feedBuilder;
 
     private NycTransitDataService _transitDataService;
+
+    private ConfigurationService _configurationService;
 
     @Autowired
     public void setFeedBuilder(ServiceAlertFeedBuilder feedBuilder) {
@@ -47,9 +56,30 @@ public class ServiceAlertServiceImpl extends AbstractFeedMessageService {
         _transitDataService = transitDataService;
     }
 
+    @Autowired
+    public void setConfigurationService(ConfigurationService configurationService) {
+        _configurationService = configurationService;
+    }
+
+    @PostConstruct
+    @Override
+    public void init() {
+        refreshConfig();
+        super.init();
+    }
+
+    @Refreshable(dependsOn = CONFIG_ALERTS_REFRESH_INTERVAL)
+    private void refreshConfig() {
+        int interval = Integer.parseInt(
+                _configurationService.getConfigurationValueAsString(
+                        CONFIG_ALERTS_REFRESH_INTERVAL,
+                        String.valueOf(DEFAULT_REFRESH_INTERVAL_SECONDS)));
+        setRefreshIntervalSeconds(interval);
+    }
+
     @Override
     public List<FeedEntity.Builder> getEntities(long time) {
-        List<FeedEntity.Builder> ret = new ArrayList<FeedEntity.Builder>();
+        List<FeedEntity.Builder> ret = new ArrayList<>();
         for (AgencyWithCoverageBean ab : _transitDataService.getAgenciesWithCoverage()) {
             String agencyId = ab.getAgency().getId();
             ListBean<ServiceAlertBean> serviceAlertBeans = _transitDataService.getAllServiceAlertsForAgencyId(agencyId);
